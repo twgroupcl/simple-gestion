e790f63d4f7d1f8b7eded7fa4f14fa6a92667ea1
Jorge Castro <jorge.castro@twgroup.cl>
2020-08-29 19:50:30 -0400

    Add Attribute Scopes

---
+++ app/Scopes/CompanyBranchScope.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Schema;
+
+class CompanyBranchScope implements Scope
+{
+    public function apply(Builder $builder, Model $model)
+    {
+    	if (app()->runningInConsole()) {
+    		return;
+    	}
+
+    	$user = backpack_user();
+
+    	$table = $model->getTable();
+
+		$hasCompanyColumn = Schema::connection('mysql')->hasColumn($model->getTable(), 'company_id');
+
+		$hasBranchColumn = Schema::connection('mysql')->hasColumn($model->getTable(), 'branch_id');
+
+		if ($hasCompanyColumn) {
+			$builder->where($table.'.company_id', $user->current()->company->id);
+		}
+
+		if ($hasBranchColumn) {
+			$builder->where($table.'.branch_id', $user->current()->branch->id);
+		}
+    }
+}
