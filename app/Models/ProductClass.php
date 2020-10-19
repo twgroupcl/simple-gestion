8cdbf6a4724affd4d0ea65f21cc471195cdac20a
jose dlucca <jose.dlucca@twgroup.cl>
2020-10-19 14:31:19 -0700

    ProductClass model

--- app/Models/ProductClass.php
+++ app/Models/ProductClass.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use App\Scopes\CompanyBranchScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 class ProductClass extends Model
 {
     use CrudTrait;
+    use SoftDeletes;
+
+    const STATUS_ACTIVE = 1;
+    const STATUS_INACTIVE = 0;
 
     /*
     |--------------------------------------------------------------------------
@@ -23,17 +29,38 @@ class ProductClass extends Model
     // protected $hidden = [];
     // protected $dates = [];
 
+
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new CompanyBranchScope);
+    }
+
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
     |--------------------------------------------------------------------------
     */
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function product_class_attributes() {
+        return $this->hasMany(ProductClassAttribute::class);
+    }
+
+    public function category() {
+        return $this->belongsTo(ProductCategory::class);
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -46,6 +73,19 @@ class ProductClass extends Model
     | ACCESSORS
     |--------------------------------------------------------------------------
     */
+    public function getStatusDescriptionAttribute()
+    {
+        switch ($this->status) {
+            case $this::STATUS_ACTIVE:
+                return 'Activo';
+                break;
+            case $this::STATUS_INACTIVE:
+                return 'Inactivo';
+                break;
+            default:
+                break;
+        }
+    }
 
     /*
     |--------------------------------------------------------------------------
