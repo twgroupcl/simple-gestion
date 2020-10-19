5383779e0bc44777549eacbd3c97ccc07a504ff4
Jorge Castro <jorge.castro@twgroup.cl>
2020-10-19 09:16:21 -0300

    Productos migrations

---
+++ database/migrations/2020_09_24_083320_create_product_types_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_types');
+    }
+}
---
+++ database/migrations/2020_09_24_095700_create_product_inventory_sources_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductInventorySourcesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_inventory_sources', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('address_street');
+            $table->string('address_number');
+            $table->string('address_subnumber')->nullable();
+            $table->unsignedBigInteger('commune_id');
+            $table->string('contact_uid')->nullable();
+            $table->string('contact_first_name')->nullable();
+            $table->string('contact_last_name')->nullable();
+            $table->string('contact_email')->nullable();
+            $table->string('contact_phone')->nullable();
+            $table->integer('priority')->default(0);
+            $table->decimal('latitude', 10, 5)->nullable();
+            $table->decimal('longitude', 10, 5)->nullable();
+            $table->integer('status')->default(1);
+            $table->unsignedBigInteger('business_id');
+            $table->timestamps();
+
+            $table->softDeletes();
+        });
+
+        Schema::table('product_inventory_sources', function (Blueprint $table) {
+            $table->foreign('commune_id')->references('id')->on('communes');
+            $table->foreign('business_id')->references('id')->on('businesses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_inventory_sources');
+    }
+}
