9585b89f94b8c260475a9d02cc45814dfc7dff3b
jose dlucca <jose.dlucca@twgroup.cl>
2020-10-20 08:09:18 -0700

    ProductInventorySource crud

--- app/Http/Controllers/Admin/ProductInventorySourceCrudController.php
+++ app/Http/Controllers/Admin/ProductInventorySourceCrudController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Cruds\BaseCrudFields;
 use App\Http\Requests\ProductInventorySourceRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
@@ -28,7 +29,8 @@ class ProductInventorySourceCrudController extends CrudController
     {
         CRUD::setModel(\App\Models\ProductInventorySource::class);
         CRUD::setRoute(config('backpack.base.route_prefix') . '/productinventorysource');
-        CRUD::setEntityNameStrings('productinventorysource', 'product_inventory_sources');
+        CRUD::setEntityNameStrings('bodega', 'bodegas');
+        $this->crud->denyAccess('show');
     }
 
     /**
@@ -39,13 +41,40 @@ class ProductInventorySourceCrudController extends CrudController
      */
     protected function setupListOperation()
     {
-        CRUD::setFromDb(); // columns
+        //CRUD::setFromDb(); // columns
 
-        /**
-         * Columns can be defined using the fluent syntax or array syntax:
-         * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
-         */
+        CRUD::addColumn([
+            'name' => 'code',
+            'label' => 'Codigo',
+            'type' => 'text',
+        ]);
+        
+        CRUD::addColumn([
+            'name' => 'name',
+            'label' => 'Nombre',
+            'type' => 'text',
+        ]);
+
+        CRUD::addColumn([
+            'name' => 'commune',
+            'label' => 'Comuna',
+            'type' => 'relationship',
+        ]);
+
+        CRUD::addColumn([
+            'name' => 'status_description',
+            'label' => 'Estado',
+            'type' => 'text',
+            'wrapper' => [
+                'element' => 'span',
+                'class' => function ($crud, $column, $entry, $related_key) {
+                    if ($column['text'] == 'Activo') {
+                        return 'badge badge-success';
+                    }
+                    return 'badge badge-default';
+                },
+            ],
+        ]);
     }
 
     /**
@@ -58,13 +87,169 @@ class ProductInventorySourceCrudController extends CrudController
     {
         CRUD::setValidation(ProductInventorySourceRequest::class);
 
-        CRUD::setFromDb(); // fields
+        $this->crud = (new BaseCrudFields())->setBaseFields($this->crud);
 
-        /**
-         * Fields can be defined using the fluent syntax or array syntax:
-         * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
-         */
+        //CRUD::setFromDb(); // fields
+
+        CRUD::addField([
+            'name' => 'name',
+            'label' => 'Nombre de la bodega',
+            'type' => 'text',
+            'tab' => 'Información general',
+        ]);
+
+        CRUD::addField([
+            'name' => 'code',
+            'label' => 'Codigo',
+            'type' => 'text',
+            'tab' => 'Información general',
+        ]);
+
+        CRUD::addField([
+            'name' => 'description',
+            'label' => 'Descripción',
+            'type' => 'textarea',
+            'tab' => 'Información general',
+        ]);
+
+        CRUD::addField([
+            'name' => 'commune_id',
+            'label' => 'Comuna',
+            'entity' => 'commune',
+            'type' => 'relationship',
+            'tab' => 'Ubicación',
+        ]);
+
+
+        CRUD::addField([
+            'name' => 'address_street',
+            'label' => 'Calle',
+            'type' => 'text',
+            'tab' => 'Ubicación',
+            'wrapper' => [
+                'class' => 'form-group col-lg-6 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'address_number',
+            'label' => 'Numero',
+            'type' => 'text',
+            'tab' => 'Ubicación',
+            'wrapper' => [
+                'class' => 'form-group col-lg-3 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'address_subnumber',
+            'label' => 'Casa/Depto/Oficina',
+            'type' => 'text',
+            'tab' => 'Ubicación',
+            'wrapper' => [
+                'class' => 'form-group col-lg-3 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'priority',
+            'label' => 'Prioridad de la bodega',
+            'type' => 'number',
+            'tab' => 'Información general',
+            'wrapper' => [
+                'class' => 'form-group col-lg col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'latitude',
+            'label' => 'Latitud',
+            'type' => 'number',
+            'attributes' => [
+                'step' => 'any',
+            ],
+            'tab' => 'Ubicación',
+            'wrapper' => [
+                'class' => 'form-group col-lg col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'longitude',
+            'label' => 'Longitud',
+            'type' => 'number',
+            'tab' => 'Ubicación',
+            'attributes' => [
+                'step' => 'any',
+            ],
+            'wrapper' => [
+                'class' => 'form-group col-lg col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'status',
+            'label' => 'Activo',
+            'type' => 'checkbox',
+            'tab' => 'Información general',
+            'default' => '1',
+        ]);
+
+        CRUD::addField([
+            'name' => 'contact_uid',
+            'label' => 'RUT',
+            'type' => 'text',
+            'tab' => 'Información de contacto',
+        ]);
+
+        CRUD::addField([
+            'name' => 'contact_first_name',
+            'label' => 'Nombre',
+            'type' => 'text',
+            'tab' => 'Información de contacto',
+            'wrapper' => [
+                'class' => 'form-group col-lg-6 col-md-12 mb-3',
+            ],
+        ]);
+
+
+        CRUD::addField([
+            'name' => 'contact_last_name',
+            'label' => 'Apellido',
+            'type' => 'text',
+            'tab' => 'Información de contacto',
+            'wrapper' => [
+                'class' => 'form-group col-lg-6 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'contact_email',
+            'label' => 'Correo electronico',
+            'type' => 'email',
+            'tab' => 'Información de contacto',
+            'wrapper' => [
+                'class' => 'form-group col-lg-6 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'contact_phone',
+            'label' => 'Numero telefonico',
+            'type' => 'text',
+            'tab' => 'Información de contacto',
+            'wrapper' => [
+                'class' => 'form-group col-lg-6 col-md-12 mb-3',
+            ],
+        ]);
+
+        CRUD::addField([
+            'name' => 'rut_formatter',
+            'type' => 'rut_formatter',
+            'rut_fields' => ['contact_uid'],
+            'tab' => 'Ubicación',
+        ]);
+
     }
 
     /**
