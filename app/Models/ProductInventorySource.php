86226917f6631d1e09d1bc74e4bfecfb49c0b53f
jose dlucca <jose.dlucca@twgroup.cl>
2020-10-20 08:08:48 -0700

    ProductInventorySource model

--- app/Models/ProductInventorySource.php
+++ app/Models/ProductInventorySource.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
-use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use App\Scopes\CompanyBranchScope;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 
 class ProductInventorySource extends Model
 {
     use CrudTrait;
+    use SoftDeletes;
 
     /*
     |--------------------------------------------------------------------------
@@ -23,17 +26,41 @@ class ProductInventorySource extends Model
     // protected $hidden = [];
     // protected $dates = [];
 
+    public const STATUS_ACTIVE = 1;
+    public const STATUS_INACTIVE = 0;
+
     /*
     |--------------------------------------------------------------------------
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new CompanyBranchScope);
+    }
 
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
     |--------------------------------------------------------------------------
     */
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function commune()
+    {
+        return $this->belongsTo(Commune::class);
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_inventories', 'product_inventory_source_id', 'product_id')->withPivot('qty');
+    }
 
     /*
     |--------------------------------------------------------------------------
@@ -46,6 +73,19 @@ class ProductInventorySource extends Model
     | ACCESSORS
     |--------------------------------------------------------------------------
     */
+    public function getStatusDescriptionAttribute()
+    {
+        switch ($this->status) {
+            case $this::STATUS_ACTIVE:
+                return 'Activo';
+                break;
+            case $this::STATUS_INACTIVE:
+                return 'Inactivo';
+                break;
+            default:
+                break;
+        }
+    }
 
     /*
     |--------------------------------------------------------------------------
