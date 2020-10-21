8bdf7c3d65f3dcfedc31402583e1b53a9781312c
Jorge Castro <jorge.castro@twgroup.cl>
2020-10-21 11:29:14 -0300

    Add front views for shop

--- resources/views/layouts/footer/menu.blade.php
+++ resources/views/layouts/footer/menu.blade.php
@@ -2,6 +2,17 @@
     $collaboration = (isset($collaboration)) ? $collaboration : false;
 @endphp
 
+<!-- Toast: Added to Cart-->
+<div class="toast-container toast-bottom-center">
+    <div class="toast mb-3" id="cart-toast" data-delay="5000" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header bg-success text-white"><i class="czi-check-circle mr-2"></i>
+            <h6 class="font-size-sm text-white mb-0 mr-auto">Added to cart!</h6>
+            <button class="close text-white ml-2 mb-1" type="button" data-dismiss="toast" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        </div>
+        <div class="toast-body">This item has been added to your cart.</div>
+    </div>
+</div>
+
 <!-- Footer-->
 <footer class="bg-light pt-5">
     @if($collaboration)
