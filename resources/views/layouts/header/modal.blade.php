d5f31f5fe72f26216160a495013e17e2df689a53
Jorge Castro <jorge.castro@twgroup.cl>
2020-10-21 09:52:50 -0300

    Update language and styles

--- resources/views/layouts/header/modal.blade.php
+++ resources/views/layouts/header/modal.blade.php
@@ -4,68 +4,70 @@
         <div class="modal-content">
             <div class="modal-header">
                 <ul class="nav nav-tabs card-header-tabs" role="tablist">
-                    <li class="nav-item"><a class="nav-link active" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-unlocked mr-2 mt-n1"></i>Sign in</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-user mr-2 mt-n1"></i>Sign up</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="#signin-tab" data-toggle="tab" role="tab" aria-selected="true"><i class="czi-unlocked mr-2 mt-n1"></i>Inicia sesión</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#signup-tab" data-toggle="tab" role="tab" aria-selected="false"><i class="czi-user mr-2 mt-n1"></i>Regístrate</a></li>
                 </ul>
                 <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body tab-content py-4">
                 <form class="needs-validation tab-pane fade show active" autocomplete="off" novalidate id="signin-tab">
                     <div class="form-group">
-                        <label for="si-email">Email address</label>
-                        <input class="form-control" type="email" id="si-email" placeholder="johndoe@example.com" required>
-                        <div class="invalid-feedback">Please provide a valid email address.</div>
+                        <label for="si-email">Email</label>
+                        <input class="form-control" type="email" id="si-email" placeholder="" required>
+                        <div class="invalid-feedback">Por favor ingresa un email válido.</div>
                     </div>
                     <div class="form-group">
-                        <label for="si-password">Password</label>
+                        <label for="si-password">Contraseña</label>
                         <div class="password-toggle">
                             <input class="form-control" type="password" id="si-password" required>
                             <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show
-                                    password</span>
+                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Mostrar contraseña</span>
                             </label>
                         </div>
                     </div>
                     <div class="form-group d-flex flex-wrap justify-content-between">
                         <div class="custom-control custom-checkbox mb-2">
                             <input class="custom-control-input" type="checkbox" id="si-remember">
-                            <label class="custom-control-label" for="si-remember">Remember me</label>
-                        </div><a class="font-size-sm" href="#">Forgot password?</a>
+                            <label class="custom-control-label" for="si-remember">Recordarme</label>
+                        </div><a class="font-size-sm" href="#">¿Olvidaste tu contraseña??</a>
                     </div>
-                    <button class="btn btn-primary btn-block btn-shadow" type="submit">Sign in</button>
+                    <button class="btn btn-primary btn-block btn-shadow" type="submit">Iniciar sesión</button>
                 </form>
                 <form class="needs-validation tab-pane fade" autocomplete="off" novalidate id="signup-tab">
                     <div class="form-group">
-                        <label for="su-name">Full name</label>
-                        <input class="form-control" type="text" id="su-name" placeholder="John Doe" required>
-                        <div class="invalid-feedback">Please fill in your name.</div>
+                        <label for="su-firstname">Nombre</label>
+                        <input class="form-control" type="text" id="su-firstname" placeholder="Escribe aquí tu nombre" required>
+                        <div class="invalid-feedback">Por favor ingresa tu nombre.</div>
                     </div>
                     <div class="form-group">
-                        <label for="su-email">Email address</label>
-                        <input class="form-control" type="email" id="su-email" placeholder="johndoe@example.com" required>
-                        <div class="invalid-feedback">Please provide a valid email address.</div>
+                        <label for="su-lastname">Apellido</label>
+                        <input class="form-control" type="text" id="su-lastname" placeholder="Escribe aquí tu apellido" required>
+                        <div class="invalid-feedback">Por favor ingresa tu apellido.</div>
                     </div>
                     <div class="form-group">
-                        <label for="su-password">Password</label>
+                        <label for="su-email">Email</label>
+                        <input class="form-control" type="email" id="su-email" placeholder="Escribe aquí tu email" required>
+                        <div class="invalid-feedback">Por favor ingresa un email válido.</div>
+                    </div>
+                    <div class="form-group">
+                        <label for="su-password">Contraseña</label>
                         <div class="password-toggle">
                             <input class="form-control" type="password" id="su-password" required>
                             <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show
-                                    password</span>
+                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Mostrar contraseña</span>
                             </label>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="su-password-confirm">Confirm password</label>
+                        <label for="su-password-confirm">Confirmar contraseña</label>
                         <div class="password-toggle">
                             <input class="form-control" type="password" id="su-password-confirm" required>
                             <label class="password-toggle-btn">
-                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Show
-                                    password</span>
+                                <input class="custom-control-input" type="checkbox"><i class="czi-eye password-toggle-indicator"></i><span class="sr-only">Mostrar contraseña</span>
                             </label>
                         </div>
                     </div>
-                    <button class="btn btn-primary btn-block btn-shadow" type="submit">Sign up</button>
+                    <button class="btn btn-primary btn-block btn-shadow" type="submit">Registrar</button>
                 </form>
             </div>
         </div>
