97e23b417bea5add95a9aa763f71c618b6635077
Julian Murphy <julian.murphy@twgroup.cl>
2020-10-21 22:27:34 -0300

    dropdown items

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -7,52 +7,6 @@
         <small>Carro</small>$1,247.00
     </a>
     <!-- Cart dropdown-->
-    <div class="dropdown-menu dropdown-menu-right" style="width: 20rem;">
-        <div class="widget widget-cart px-3 pt-2 pb-3">
-            <div style="height: 15rem;" data-simplebar data-simplebar-auto-hide="false">
-                <div class="widget-cart-item pb-2 border-bottom">
-                    <button class="close text-danger" type="button" aria-label="Remove"><span aria-hidden="true">&times;</span></button>
-                    <div class="media align-items-center"><a class="d-block mr-2" href="shop-single-v2.html"><img width="64" src="{{ asset('img/shop/cart/widget/05.jpg') }}" alt="Product" /></a>
-                        <div class="media-body">
-                            <h6 class="widget-product-title"><a href="shop-single-v2.html">Bluetooth Headphones</a></h6>
-                            <div class="widget-product-meta"><span class="text-accent mr-2">$259.<small>00</small></span><span class="text-muted">x 1</span></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="widget-cart-item py-2 border-bottom">
-                    <button class="close text-danger" type="button" aria-label="Remove"><span aria-hidden="true">&times;</span></button>
-                    <div class="media align-items-center"><a class="d-block mr-2" href="shop-single-v2.html"><img width="64" src="{{ asset('img/shop/cart/widget/06.jpg') }}" alt="Product" /></a>
-                        <div class="media-body">
-                            <h6 class="widget-product-title"><a href="shop-single-v2.html">Cloud
-                                    Security Camera</a></h6>
-                            <div class="widget-product-meta"><span class="text-accent mr-2">$122.<small>00</small></span><span class="text-muted">x 1</span></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="widget-cart-item py-2 border-bottom">
-                    <button class="close text-danger" type="button" aria-label="Remove"><span aria-hidden="true">&times;</span></button>
-                    <div class="media align-items-center"><a class="d-block mr-2" href="shop-single-v2.html"><img width="64" src="{{ asset('img/shop/cart/widget/07.jpg') }}" alt="Product" /></a>
-                        <div class="media-body">
-                            <h6 class="widget-product-title"><a href="shop-single-v2.html">Android Smartphone S10</a></h6>
-                            <div class="widget-product-meta"><span class="text-accent mr-2">$799.<small>00</small></span><span class="text-muted">x 1</span></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="widget-cart-item py-2 border-bottom">
-                    <button class="close text-danger" type="button" aria-label="Remove"><span aria-hidden="true">&times;</span></button>
-                    <div class="media align-items-center"><a class="d-block mr-2" href="shop-single-v2.html"><img width="64" src="{{ asset('img/shop/cart/widget/08.jpg') }}" alt="Product" /></a>
-                        <div class="media-body">
-                            <h6 class="widget-product-title"><a href="shop-single-v2.html">Android Smart TV Box</a></h6>
-                            <div class="widget-product-meta"><span class="text-accent mr-2">$67.<small>00</small></span><span class="text-muted">x 1</span></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex flex-wrap justify-content-between align-items-center py-3">
-                <div class="font-size-sm mr-2 py-2"><span class="text-muted">Subtotal:</span><span class="text-accent font-size-base ml-1">$1,247.<small>00</small></span>
-                </div><a class="btn btn-outline-secondary btn-sm" href="shop-cart.html">Expand
-                    cart<i class="czi-arrow-right ml-1 mr-n1"></i></a>
-            </div><a class="btn btn-primary btn-sm btn-block" href="checkout-details.html"><i class="czi-card mr-2 font-size-base align-middle"></i>Checkout</a>
-        </div>
-    </div>
+    @livewire('cart.dropdown')
+    
 </div>
