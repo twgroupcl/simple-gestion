39be6f4054c59b34182b7eb86f623ec60993207b
Jorge Castro <jorge.castro@twgroup.cl>
2020-10-22 12:31:03 -0300

    Fix category icons, fix dashboard Roberto's rarely variable declaration, change scripts to base template

---
+++ resources/views/layouts/footer/top.blade.php
@@ -0,0 +1,4 @@
+<!-- Back To Top Button-->
+<a class="btn-scroll-top" href="#top" data-scroll>
+    <span class="btn-scroll-top-tooltip text-muted font-size-sm mr-2">Top</span><i class="btn-scroll-top-icon czi-arrow-up"></i>
+</a>
--- resources/views/livewire/categories-menu.blade.php
+++ resources/views/livewire/categories-menu.blade.php
@@ -1,9 +1,12 @@
 <ul class="navbar-nav mega-nav pr-lg-2 mr-lg-2">
-    <li class="nav-item dropdown"><a class="nav-link dropdown-toggle pl-0" href="#" data-toggle="dropdown"><i class="czi-menu align-middle mt-n1 mr-2"></i>Categorías</a>
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle pl-0" href="#" data-toggle="dropdown">
+            <i class="czi-menu align-middle mt-n1 mr-2"></i>Categorías
+        </a>
         <ul class="dropdown-menu">
             @foreach ($categories as $category)
             <li class="dropdown mega-dropdown">
-                <a class="dropdown-item dropdown-toggle" href="#" data-toggle="dropdown">
+                <a class="dropdown-item" href="#">
                     <i class="{{ $category->icon }} opacity-60 font-size-lg mt-n1 mr-2"></i> {{ $category->name }}
                 </a>
             </li>
