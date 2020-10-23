87964b4289e30fa6db8ffe663cc2f10ac00c4705
Julian Murphy <julian.murphy@twgroup.cl>
2020-10-23 15:31:05 -0300

    fix pointer

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -3,7 +3,7 @@
     
     <livewire:cart.cart-counter/>
 
-    <a class="navbar-tool-text" href="{{ $cursor === 'auto' ? route('shopping-cart') : '#'}}"  style="cursor:{{ $cursor }};">
+    <a class="navbar-tool-text" href="{{ $cursor === 'auto' ? route('shopping-cart') : '#'}}"  style="cursor:{{ $cursor == 'auto' ? 'pointer' : $cursor }};">
         <small>Carro</small>{{ currencyFormat($subtotal, Setting::get('default_currency'), true) }}
     </a>
 
