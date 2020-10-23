313928b8e33d49aa3d6bc5c0c2bdcb0883051a45
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-10-23 11:03:33 -0500

    Show customer orders

--- resources/views/customer/order.blade.php
+++ resources/views/customer/order.blade.php
@@ -58,42 +58,18 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">34VB5540K83</a></td>
-                            <td class="py-3">May 21, 2019</td>
-                            <td class="py-3"><span class="badge badge-info m-0">In Progress</span></td>
-                            <td class="py-3">$358.75</td>
-                        </tr>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">78A643CD409</a></td>
-                            <td class="py-3">December 09, 2018</td>
-                            <td class="py-3"><span class="badge badge-danger m-0">Canceled</span></td>
-                            <td class="py-3"><span>$760.50</span></td>
-                        </tr>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">112P45A90V2</a></td>
-                            <td class="py-3">October 15, 2018</td>
-                            <td class="py-3"><span class="badge badge-warning m-0">Delayed</span></td>
-                            <td class="py-3">$1,264.00</td>
-                        </tr>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">28BA67U0981</a></td>
-                            <td class="py-3">July 19, 2018</td>
-                            <td class="py-3"><span class="badge badge-success m-0">Delivered</span></td>
-                            <td class="py-3">$198.35</td>
-                        </tr>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">502TR872W2</a></td>
-                            <td class="py-3">April 04, 2018</td>
-                            <td class="py-3"><span class="badge badge-success m-0">Delivered</span></td>
-                            <td class="py-3">$2,133.90</td>
-                        </tr>
-                        <tr>
-                            <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">47H76G09F33</a></td>
-                            <td class="py-3">March 30, 2018</td>
-                            <td class="py-3"><span class="badge badge-success m-0">Delivered</span></td>
-                            <td class="py-3">$86.40</td>
-                        </tr>
+                        @forelse ($customer->orders as $order)
+                            <tr>
+                                <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">{{ $order->id }}</a></td>
+                                <td class="py-3">{{ $order->created_at }}</td>
+                                <td class="py-3"><span class="badge badge-info m-0">{{ $order->order_status ?? 'Sin estado' }}</span></td>
+                                <td class="py-3">{{ $order->total }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm">No has ordenado nada aún</a></td>
+                            </tr>
+                        @endforelse
                     </tbody>
                 </table>
             </div>
