fb7bc802b8cf560a3c1705e3961b77ca88d9d25c
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-10-23 11:07:01 -0500

    Format created_at value

--- resources/views/customer/order.blade.php
+++ resources/views/customer/order.blade.php
@@ -61,7 +61,7 @@
                         @forelse ($customer->orders as $order)
                             <tr>
                                 <td class="py-3"><a class="nav-link-style font-weight-medium font-size-sm" href="#order-details" data-toggle="modal">{{ $order->id }}</a></td>
-                                <td class="py-3">{{ $order->created_at }}</td>
+                                <td class="py-3">{{ $order->created_at->format('d-m-Y') }}</td>
                                 <td class="py-3"><span class="badge badge-info m-0">{{ $order->order_status ?? 'Sin estado' }}</span></td>
                                 <td class="py-3">{{ currencyFormat($order->total, 'CLP', true) }}</td>
                             </tr>
