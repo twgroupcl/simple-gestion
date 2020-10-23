2a736a449cbc77994816bc7dcb3e4f5d4f00715f
Julio Barragan <jcbarragan1994@gmail.com>
2020-10-23 09:46:56 -0400

    card, home controller

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         //return redirect('/seller/register');
-        $products = Product::where('status','=','1')->with('seller')->with('seller')->with('categories')->limit(6)->get();
+        $products = Product::where('status','=','1')->with('seller')->with('categories')->orderBy('id','DESC')->limit(6)->get();
         return view('marketplace', compact('products'));
     }
 
