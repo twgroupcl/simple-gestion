edd07dac07a6aa17925739f31385ccb5e3587d2d
Julian Murphy <julian.murphy@twgroup.cl>
2020-10-23 00:13:59 -0300

    more orthography and fix icon size in dropdown

--- resources/views/livewire/cart/dropdown-item.blade.php
+++ resources/views/livewire/cart/dropdown-item.blade.php
@@ -2,10 +2,10 @@
     <div class="widget-cart-item pb-2 border-bottom">
         @if ($confirm == $item->id)
             <button class="close text-danger" wire:click.prevent="delete" type="button" aria-label="Remove"><span aria-hidden="true"><i
-                class="czi-trash"></i></span></button>
+                class="czi-trash font-size-sm"></i></span></button>
         @else
             <button class="close text-danger" wire:click.prevent="deleteConfirm({{$item->id}})" type="button" aria-label="Remove"><span aria-hidden="true"><i
-                class="czi-close-circle"></i></span></button>
+                class="czi-close-circle font-size-sm"></i></span></button>
         @endif
         <div class="media align-items-center">
             <a class="d-block mr-2" href="{{route('product',['slug' => $product->url_key])}}"><img width="64" src="{{ url($product->getFirstImagePath()) }}" alt="Product" /></a>
