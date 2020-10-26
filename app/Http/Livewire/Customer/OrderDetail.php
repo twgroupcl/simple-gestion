4b114ade9e39f58ee594e26a43c74a515604dcaa
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-10-26 13:07:19 -0500

    Change component to show order Details in popup

--- app/Http/Livewire/Customer/OrderDetail.php
+++ app/Http/Livewire/Customer/OrderDetail.php
@@ -2,12 +2,30 @@
 
 namespace App\Http\Livewire\Customer;
 
+use App\Models\Order;
+use App\Models\OrderItem;
 use Livewire\Component;
 
 class OrderDetail extends Component
 {
+    public $order;
+    public $items;
+
+    protected $listeners = ['refreshOrderDetail' => 'refresh'];
+
     public function render()
     {
-        return view('livewire.customer.order-detail');
+        // dd($this->items);
+        return view('livewire.customer.order-detail', [
+            'order' => $this->order,
+            'items' => $this->items,
+        ]);
+    }
+
+    public function refresh($order_id)
+    {
+        $this->order = Order::find($order_id);
+        $this->items = OrderItem::where('order_id', $order_id)->get();
+        // dd($this->items);
     }
 }
