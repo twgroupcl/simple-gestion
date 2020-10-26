7677c4c0b2df979bce2bd19aa687ae653ff0d02a
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-10-26 10:28:41 -0500

    Create livewire component for order details

---
+++ app/Http/Livewire/Customer/OrderDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Customer;
+
+use Livewire\Component;
+
+class OrderDetail extends Component
+{
+    public function render()
+    {
+        return view('livewire.customer.order-detail');
+    }
+}
