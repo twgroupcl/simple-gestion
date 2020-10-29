e55add725368c1a7f2def573151fb6408b567721
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-10-29 02:13:45 -0500

    Update customer addresses

--- app/Http/Livewire/Customer/AddressForm.php
+++ app/Http/Livewire/Customer/AddressForm.php
@@ -10,9 +10,24 @@ class AddressForm extends Component
     public $customer;
     public $communes;
     public $address;
+    public $oldAddress;
 
     protected $listeners = [
         'loadUpdateForm' => 'loadForm',
+    ];
+
+    protected $rules = [
+        "address.street" => 'required',
+        "address.number" => 'required',
+        "address.subnumber" => 'nullable',
+        "address.commune_id" => "required",
+        'address.uid' => 'nullable',
+        "address.first_name" => 'nullable',
+        "address.last_name" => 'nullable',
+        "address.email" => 'nullable',
+        "address.phone" => 'nullable',
+        "address.cellphone" => 'nullable',
+        "address.extra" => 'nullable',
     ];
 
     public function render()
@@ -34,9 +49,31 @@ class AddressForm extends Component
     {
         $this->dispatchBrowserEvent('modal-form');
         $this->address = $address;
+        $this->oldAddress = $address;
     }
 
     public function save()
     {
+        $this->validate();
+
+        $addresses_data = collect(
+            is_array($this->customer->addresses_data)
+                ? $this->customer->addresses_data
+                : json_decode($this->customer->addresses_data, true) ?? []
+        );
+
+        $target = $addresses_data->search(
+            $this->oldAddress
+        );
+
+        $update = $addresses_data->replace([
+            $target => $this->address
+        ]);
+
+        $this->customer->update([
+            'addresses_data' => $update->toJson(),
+        ]);
+
+        $this->dispatchBrowserEvent('close-modal-form');
     }
 }
--- resources/views/livewire/customer/address-form.blade.php
+++ resources/views/livewire/customer/address-form.blade.php
@@ -12,21 +12,21 @@
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="street">Calle <span class="text-danger">*</span></label>
-                            <input class="form-control" value="{{ $address['street'] ?? '' }}" name="street" type="text" id="street" required>
+                            <input class="form-control" wire:model="address.street" name="street" type="text" id="street" required>
                             <div class="invalid-feedback">Escriba la calle</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="number">Número <span class="text-danger">*</span></label>
-                            <input class="form-control" value="{{ $address['number'] ?? '' }}" name="number" type="text" id="number" required>
+                            <input class="form-control" wire:model="address.number" name="number" type="text" id="number" required>
                             <div class="invalid-feedback">Escriba el número!</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="address-company">Casa/Dpto/Oficina</label>
-                            <input class="form-control" type="text" value="{{ $address['subnumber'] ?? '' }}" name="subnumber" id="address-company">
+                            <input class="form-control" type="text" wire:model="address.subnumber" name="subnumber" id="address-company">
                         </div>
                     </div>
                     <div class="col-sm-6">
@@ -43,49 +43,49 @@
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="uid">RUT</label>
-                            <input class="form-control" value="{{ $address['uid'] ?? '' }}" name="uid" type="text" id="uid">
+                            <input class="form-control" wire:model="address.uid" name="uid" type="text" id="uid">
                             <div class="invalid-feedback">Escriba el Rut</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="first_name">Nombre</label>
-                            <input class="form-control" value="{{ $address['first_name'] ?? '' }}" name="first_name" type="text" id="first_name">
+                            <input class="form-control" wire:model="address.first_name" name="first_name" type="text" id="first_name">
                             <div class="invalid-feedback">Escriba el nombre</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="last_name">Apellido</label>
-                            <input class="form-control" value="{{ $address['last_name'] ?? '' }}" name="last_name" type="text" id="last_name">
+                            <input class="form-control" wire:model="address.last_name" name="last_name" type="text" id="last_name">
                             <div class="invalid-feedback">Escriba el apellido</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input class="form-control" value="{{ $address['email'] ?? '' }}" name="email" type="text" id="email">
+                            <input class="form-control" wire:model="address.email" name="email" type="text" id="email">
                             <div class="invalid-feedback">Escriba el email</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="phone">Teléfono</label>
-                            <input class="form-control" value="{{ $address['phone'] ?? '' }}" name="phone" type="text" id="phone">
+                            <input class="form-control" wire:model="address.phone" name="phone" type="text" id="phone">
                             <div class="invalid-feedback">Escriba el teléfono</div>
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="cellphone">Teléfono móvil</label>
-                            <input class="form-control" value="{{ $address['cellphone'] ?? '' }}" name="cellphone" type="text" id="cellphone">
+                            <input class="form-control" wire:model="address.cellphone" name="cellphone" type="text" id="cellphone">
                             <div class="invalid-feedback">Escriba el teléfono móvil</div>
                         </div>
                     </div>
                     <div class="col-sm-12">
                         <div class="form-group">
                             <label for="extra">Detalles</label>
-                            <textarea class="form-control" value="{{ $address['extra'] ?? '' }}" name="extra" id="extra"></textarea>                            <div class="invalid-feedback">Escriba el teléfono móvil</div>
+                            <textarea class="form-control" wire:model="address.extra" name="extra" id="extra"></textarea>                            <div class="invalid-feedback">Escriba el teléfono móvil</div>
                         </div>
                     </div>
                 </div>
@@ -106,6 +106,10 @@
             $("#update-address").modal();
             let value = @this.address['commune_id']
             $('#update-address').find(`option[value="${value}"]`).prop('selected', 'selected').change();
+        })
+
+        window.addEventListener('close-modal-form', event => {
+            $("#update-address").modal('hide');
         })
     </script>
 @endpush
