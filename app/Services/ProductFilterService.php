717fe485c6fcc9e6f25b102d3592143b6d5dc456
Jose Alejandro <dluccajose@gmail.com>
2020-10-29 14:29:22 -0400

    added brand filter and fix attributes filter

--- app/Services/ProductFilterService.php
+++ app/Services/ProductFilterService.php
@@ -11,9 +11,9 @@ class ProductFilterService {
         $query = Product::query();
 
         // Category filter
-        $query = $query->whereHas('categories', function($q) {
+        /* $query = $query->whereHas('categories', function($q) {
             return $q->where('id', 1);
-        });
+        }); */
 
         // Price filter
         if ( isset($data['price']) ) {
@@ -22,6 +22,17 @@ class ProductFilterService {
             $query = $query->where('price', '<', $priceRange[1]);
 
             $query = $query->where('price', '>', $priceRange[0]);
+        }
+
+        // Brand Filter
+        if ( isset($data['brand']) ) {
+            $brands = explode(',', $data['brand']);
+
+            $query = $query->where(function ($q) use ($brands) {
+                foreach ($brands as $brandId) {
+                    $q->orWhere('product_brand_id', $brandId);
+                }
+            });
         }
 
         // Filtro ubicacion
@@ -42,14 +53,31 @@ class ProductFilterService {
             }
         }
 
-        foreach($customAttributes as $id => $value) {
-            $query = $query->whereHas('custom_attributes', function($q) use ($id, $value) {
-                $q->where([ 
-                    'product_class_attribute_id' => $id,
-                    'json_value' => $value,
-                ]);
+        // Filter by every attribute
+        foreach($customAttributes as $id => $values) {
+            
+            // Separate mulitples values into an array
+            $values_array = explode('|', $values);
+            $query = $query->whereHas('custom_attributes', function($q) use ($id, $values_array) {
+                
+                // Main where
+                $q->where(function ($q2) use ($id, $values_array) {
+                    
+                    // Add an orWhere for every value of an attribute
+                    foreach($values_array as $value) {
+                        $q2->orWhere(function ($q3) use ($id, $value) {
+                            $q3->where([ 
+                                'product_class_attribute_id' => $id,
+                                'json_value' => $value,
+                            ]);
+                        });
+                    }
+                });
             });
         }
+
+       // dd($query->toSql(), $query->getBindings());
+       // dd($query->toSql());
 
         return $query;
     }
