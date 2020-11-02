78c4735b283846491837fcd2fbd6d2a6ab3ac41f
Julio Barragan <jcbarragan1994@gmail.com>
2020-11-02 00:06:30 -0400

    search

--- resources/views/livewire/search-navbar.blade.php
+++ resources/views/livewire/search-navbar.blade.php
@@ -1,7 +1,4 @@
 <div class="input-group-overlay d-none d-lg-block mx-4">
-    @php
-        //echo $query = Request::segment(3);
-    @endphp
     <div class="input-group-prepend-overlay"><span class="input-group-text"><btn wire:click="search" class="btn-search"><i class="czi-search"></i></btn></span></div>
     <input wire:keydown.enter="search" class="form-control prepended-form-control appended-form-control input-search" wire:model="query" type="text" placeholder="Buscar productos">
     <div class="input-group-append-overlay">
