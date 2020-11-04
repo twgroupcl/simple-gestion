a19e4596b4899da494ebad43b7e1979a2479b50a
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-04 14:26:31 -0500

    Show created product reviews in list with basic attributes

--- app/Http/Livewire/Reviews/ReviewList.php
+++ app/Http/Livewire/Reviews/ReviewList.php
@@ -6,8 +6,17 @@ use Livewire\Component;
 
 class ReviewList extends Component
 {
+    public $product;
+    public $reviews;
+
     public function render()
     {
         return view('livewire.reviews.review-list');
+    }
+
+    public function mount($product)
+    {
+        $this->product = $product;
+        $this->reviews = $product->reviews;
     }
 }
--- resources/views/livewire/reviews/review-list.blade.php
+++ resources/views/livewire/reviews/review-list.blade.php
@@ -12,40 +12,40 @@
         </div>
     </div>
     <!-- Review-->
-    <div class="product-review pb-4 mb-4 border-bottom">
-        <div class="d-flex mb-3">
-            <div class="media media-ie-fix align-items-center mr-4 pr-2"><img class="rounded-circle"
-                                                                              width="50"
-                                                                              src="img/shop/reviews/01.jpg"
-                                                                              alt="Rafael Marquez"/>
-                <div class="media-body pl-3">
-                    <h6 class="font-size-sm mb-0">Rafael Marquez</h6><span class="font-size-ms text-muted">{{ today()->diffForHumans() }}</span>
+    @forelse ($reviews as $review)
+        <div class="product-review pb-4 mb-4 border-bottom">
+            <div class="d-flex mb-3">
+                <div class="media media-ie-fix align-items-center mr-4 pr-2">
+                    {{-- <img class="rounded-circle"
+                                                                                width="50"
+                                                                                src="img/shop/reviews/01.jpg"
+                                                                                alt="Rafael Marquez"/> --}}
+                    <div class="media-body pl-3">
+                        <h6 class="font-size-sm mb-0">{{ $review->customer->first_name }}</h6><span class="font-size-ms text-muted">{{ $review->created_at->diffForHumans() }}</span>
+                    </div>
+                </div>
+                <div>
+                    <div class="star-rating"><i class="sr-star czi-star-filled active"></i><i
+                            class="sr-star czi-star-filled active"></i><i
+                            class="sr-star czi-star-filled active"></i><i
+                            class="sr-star czi-star-filled active"></i><i class="sr-star czi-star"></i>
+                    </div>
+                    <div class="font-size-ms text-muted">83% de los usuarios consideraron útil esta revisión</div>
                 </div>
             </div>
-            <div>
-                <div class="star-rating"><i class="sr-star czi-star-filled active"></i><i
-                        class="sr-star czi-star-filled active"></i><i
-                        class="sr-star czi-star-filled active"></i><i
-                        class="sr-star czi-star-filled active"></i><i class="sr-star czi-star"></i>
-                </div>
-                <div class="font-size-ms text-muted">83% de los usuarios consideraron útil esta revisión</div>
+            <p class="font-size-md mb-2">{{ $review->comment }}</p>
+            <ul class="list-unstyled font-size-ms pt-1">
+                <li class="mb-1"><span class="font-weight-medium">Pros:&nbsp;</span>{{ $review->pros }}</li>
+                <li class="mb-1"><span class="font-weight-medium">Contras:&nbsp;</span>{{ $review->cons }}</li>
+            </ul>
+            <div class="text-nowrap">
+                <button class="btn-like" type="button">15</button>
+                <button class="btn-dislike" type="button">3</button>
             </div>
         </div>
-        <p class="font-size-md mb-2">Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil
-            impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est...</p>
-        <ul class="list-unstyled font-size-ms pt-1">
-            <li class="mb-1"><span class="font-weight-medium">Pros:&nbsp;</span>Consequuntur magni,
-                voluptatem sequi, tempora
-            </li>
-            <li class="mb-1"><span class="font-weight-medium">Contras:&nbsp;</span>Architecto beatae, quis
-                autem
-            </li>
-        </ul>
-        <div class="text-nowrap">
-            <button class="btn-like" type="button">15</button>
-            <button class="btn-dislike" type="button">3</button>
-        </div>
-    </div>
+    @empty
+        <h6>No hay opiniones</h6>
+    @endforelse
     <div class="text-center">
         <button class="btn btn-outline-accent" type="button"><i class="czi-reload mr-2"></i>Cargar más
             opiniones
