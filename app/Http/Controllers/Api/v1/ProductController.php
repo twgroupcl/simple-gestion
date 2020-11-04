353b1eae19cf6210d2e8ec892c0a02f5b4638e72
Jose Alejandro <dluccajose@gmail.com>
2020-11-04 12:48:17 -0400

    create product for every warehouse and save inventories

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -8,6 +8,8 @@ use App\Models\Product;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Services\ProductService;
+use Illuminate\Support\Facades\DB;
+use App\Models\ProductInventorySource;
 use App\Http\Controllers\Api\Controller;
 use App\Http\Requests\Api\ProductRequest;
 
@@ -34,124 +36,154 @@ class ProductController extends Controller
     public function store(ProductRequest $request)
     {   
         $this->productService = new ProductService();
-        
-        // Obtain seller id
-        if ( ! $sellerId = $this->getSellerId($request) ) {
-            return response()->json([ 
-                'status' => 'error', 
-                'message' => 'Debes indicar un campo seller_id'
-            ],  400); 
-        }
-        
-        // Validate seller id
-        $seller = Seller::find($sellerId);
-        if (! $seller) {
-            return response()->json([ 
-                'status' => 'error', 
-                'message' => 'El seller_id especificado no existe'
-            ],  404); 
-        }
 
-        // Validate SKU
-        if ( ! $this->productService->validateUniqueSku($request['sku'], $sellerId, auth()->user()->companies->first()->id) ) {
-            return response()->json([ 
-                'status' => 'error', 
-                'message' => 'Ya tienes un producto con el SKU indicado'
-            ],  400); 
-        }
+        // Obtener bodegas
+        $warehouses = json_decode($request['warehouse']);
 
-        // Validate Url key
-        $url_key = $request['url_key'] ?? Str::slug($request['name']);
+        $companyId = auth()->user()->companies->first()->id;
 
-        if ( ! $this->productService->validateUniqueSlug($url_key, auth()->user()->companies->first()->id) ) {
-            return response()->json([ 
-                'status' => 'error', 
-                'message' => 'Ya existe un producto con el url_key indicado'
-            ],  400); 
-        }
-        
-        // Set default currency
-        $currencyId = 63;
+        $products = []; 
 
-        // Validate and save custom attributes
-        
-        // Validate and save inventories
-        
-        // Save the product
-        try {
+       DB::beginTransaction();
 
-            $product  = Product::create([
-                'name' => $request['name'],
-                'sku' => $request['sku'],
-                'url_key' => $url_key,
-                'is_service' => $request['is_service'],
-                'use_inventory_control' => $request['use_inventory_control'],
-                'short_description' => $request['short_description'],
-                'description' =>  $request['description'],
-                'product_type_id' => $request['product_type_id'],
-                'product_class_id' => $request['product_class_id'],
-                'prduct_brand_id' => $request['product_brand_id'],
-                'price' => $request['price'],
-                'cost' => $request['cost'],
-                
-                'special_price' => $request['special_price'],
-                'special_price_from' => isset($request['special_price_from']) ? new DateTime($request['special_price_from']) : null,
-                'special_price_to' => isset($request['special_price_to']) ? new DateTime($request['special_price_to']) : null,
+        // Crear un producto por cada bodega
+        foreach ($warehouses as $warehouse) {
+    
+            $warehouseData = ProductInventorySource::where('code', $warehouse->code)->first();
 
-                'currency_id' => $currencyId,
+            // Obtener seller id
+            $sellerInfo = $this->getSellerIdFromWarehouse($warehouse->code);
 
-                'weight' => $request['is_service'] ? null : $request['weight'],
-                'height' => $request['is_service'] ? null : $request['height'],
-                'width' => $request['is_service'] ? null : $request['width'],
-                'depth' => $request['is_service'] ? null : $request['depth'],
-
-                'meta_title' => $request['meta_title'],
-                'meta_keywords' => $request['meta_keywords'],
-                'meta_description' => $request['meta_description'],
-                
-                'status' => $request['status'] ?? 1,
-                'seller_id' => $request['seller_id'],
-                'company_id' => auth()->user()->companies->first()->id,
-            ]);
-
-        } catch(\Illuminate\Database\QueryException $exception) {
-            return response()->json([
-                'status' => 'error',
-                'message' => $exception,
-            ], 400);
-        }
-
-        // Attach categories
-        $product->categories()->attach($request['categories']);
-
-        // Convert images to base64 and save it in the images_json field of the product
-        // so the product observer can take care of the upload
-        if ( $request->file('images') ) {
-            $imagesArray = [];
-            foreach ($request->file('images') as $image) {
-                array_push($imagesArray, ['image' => 'data:image/jpeg;base64,' . base64_encode(file_get_contents($image))]);
+            if (!$sellerInfo['status']) {
+                DB::rollBack();
+                return response()->json([ 
+                    'status' => 'error', 
+                    'message' => $sellerInfo['message'],
+                ],  400); 
             }
-            $product->images_json = $imagesArray;
+
+            $sellerId = $sellerInfo['sellerId'];
+
+            // Validate SKU
+            if ( ! $this->productService->validateUniqueSku($request['sku'], $sellerId, $companyId) ) {
+                DB::rollBack();
+                return response()->json([ 
+                    'status' => 'error', 
+                    'message' => 'Ya tienes un producto con el SKU indicado'
+                ],  400); 
+            }
+
+            // Validate Url key
+            $baseUrlKey = $request['url_key'] ?? Str::slug($request['name']);
+            $finalUrlKey = $baseUrlKey;
+            $counter = 0;
+
+            // If the Url key already exits, we added a suffix
+            while ( !$this->productService->validateUniqueSlug($finalUrlKey, $companyId) && $counter < 20) {
+                $counter++;
+                $finalUrlKey = $baseUrlKey . '-' . $counter;
+            }
+
+            if ($counter == 20) {
+                DB::rollBack();
+                return response()->json([ 
+                    'status' => 'error', 
+                    'message' => 'Ha ocurrido un error con el url_key',
+                ],  400); 
+            }
+            
+            // Set default currency
+            $currencyId = 63;
+
+            // Validate and save custom attributes
+            
+            // Validate and save inventories
+            $inventories = [];
+            $inventories['inventory-source-'.$warehouseData->id] = $warehouse->stock;
+            
+            // Save the product
+            try {
+
+                $product  = Product::create([
+                    'name' => $request['name'],
+                    'sku' => $request['sku'],
+                    'url_key' => $finalUrlKey,
+                    'is_service' => $request['is_service'],
+                    'use_inventory_control' => $request['use_inventory_control'],
+                    'short_description' => $request['short_description'],
+                    'description' =>  $request['description'],
+                    'product_type_id' => $request['product_type_id'],
+                    'product_class_id' => $request['product_class_id'],
+                    'prduct_brand_id' => $request['product_brand_id'],
+                    'price' => $warehouse->price,
+                    
+                    //'special_price' => $request['special_price'],
+                    //'special_price_from' => isset($request['special_price_from']) ? new DateTime($request['special_price_from']) : null,
+                    //'special_price_to' => isset($request['special_price_to']) ? new DateTime($request['special_price_to']) : null,
+
+                    'currency_id' => $currencyId,
+
+                    'weight' => $request['is_service'] ? null : $request['weight'],
+                    'height' => $request['is_service'] ? null : $request['height'],
+                    'width' => $request['is_service'] ? null : $request['width'],
+                    'depth' => $request['is_service'] ? null : $request['depth'],
+
+                    'meta_title' => $request['meta_title'],
+                    'meta_keywords' => $request['meta_keywords'],
+                    'meta_description' => $request['meta_description'],
+                    
+                    'status' => $request['status'] ?? 1,
+                    'seller_id' => $sellerId,
+                    'company_id' => $companyId,
+                ]);
+
+            } catch(\Illuminate\Database\QueryException $exception) {
+                DB::rollBack();
+                return response()->json([
+                    'status' => 'error',
+                    'message' => $exception,
+                ], 400);
+            }
+
+            // Attach categories
+            $product->categories()->attach($request['categories']);
+
+            // Convert images to base64 and save it in the images_json field of the product
+            // so the product observer can take care of the upload
+            if ( $request->file('images') ) {
+                $imagesArray = [];
+                foreach ($request->file('images') as $image) {
+                    array_push($imagesArray, ['image' => 'data:image/jpeg;base64,' . base64_encode(file_get_contents($image))]);
+                }
+                $product->images_json = $imagesArray;
+            }
+
+            // Update inventories and save product
+            $product->inventories_json = $inventories;
+            $product->update();
+            $products[] = $product;
         }
-        
-        $product->update();
+
+        DB::commit();
 
         return response()->json([
             'status' => 'success',
-            'data' => $product,
-            'message' => 'Producto creado exitosamente',
+            'message' => 'Productos creados correctamente',
+            'data' => $products,
         ], 200);
 
     }
 
-    private function getSellerId($request)
+    private function getSellerIdFromWarehouse($warehouseCode)
     {
-        if ( auth()->user()->hasAnyRole('Super admin|Administrador negocio|Supervisor Marketplace') ) {
-            if ( ! isset($request['seller_id']) ) return false;
-            return $request['seller_id'];
-        } else {
-            return $sellerId = Seller::where('user_id', auth()->user()->id)->first();
-        }
+
+        $warehouse = ProductInventorySource::where('code', $warehouseCode)->first();
+        if (!$warehouse) return [ 'status' => false, 'message' => 'El codigo de la bodega (' . $warehouseCode . ') no existe'];
+
+        $seller = $warehouse->branch->users->first()->seller;
+        if (!$seller) return [ 'status' => false, 'message' => 'El codigo de la bodega (' . $warehouseCode . ') no tiene asociado un vendedor'];
+
+        return [ 'sellerId' =>  $seller->id, 'status' => true];
     }
 
 }
