37ac088758121b55af172b3f52eca8c933f3ee03
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-04 12:55:41 -0500

    Add logic to validate form data

--- app/Http/Livewire/Reviews/Form.php
+++ app/Http/Livewire/Reviews/Form.php
@@ -6,8 +6,51 @@ use Livewire\Component;
 
 class Form extends Component
 {
+    public $form;
+    public $product;
+
     public function render()
     {
         return view('livewire.reviews.form');
+    }
+
+    public function mount($product)
+    {
+        $this->product = $product;
+    }
+
+    public function saveReview()
+    {
+        [$rules, $attributes, $messages] = $this->validation();
+        $this->validate($rules, $attributes, $messages);
+
+        // dd(
+        //     $this->product,
+        //     $this->form['rating'],
+        //     $this->form['comment'],
+        //     explode(',', $this->form['pros']),
+        //     explode(',', $this->form['cons'])
+        // );
+    }
+
+    public function validation()
+    {
+        return [
+            [
+                'form.rating' => 'required',
+                'form.comment' => 'required',
+                'form.pros' => 'required',
+                'form.cons' => 'required',
+            ],
+            [
+                'required' => 'Por favor complete el campo :attribute'
+            ],
+            [
+                'form.rating' => 'clasificación',
+                'form.comment' => 'opinión',
+                'form.pros' => 'pros',
+                'form.cons' => 'contras',
+            ],
+        ];
     }
 }
