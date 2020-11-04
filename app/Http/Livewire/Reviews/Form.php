7c1e8bca26124c3d79992493e6d1130c3d884c65
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-04 11:31:47 -0500

    Add review form in livewire component

---
+++ app/Http/Livewire/Reviews/Form.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Reviews;
+
+use Livewire\Component;
+
+class Form extends Component
+{
+    public function render()
+    {
+        return view('livewire.reviews.form');
+    }
+}
