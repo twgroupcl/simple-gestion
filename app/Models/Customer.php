ae3ed8d5c4cf77d0f169ebfb7e483c4bd394ce9b
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-10 13:07:50 -0500

    Add customer-log relationship

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -110,6 +110,11 @@ class Customer extends Model
         return $this->hasMany(Order::class);
     }
 
+    public function logs()
+    {
+        return $this->hasMany(CustomerLog::class);
+    }
+
     /*
     |--------------------------------------------------------------------------
     | SCOPES
