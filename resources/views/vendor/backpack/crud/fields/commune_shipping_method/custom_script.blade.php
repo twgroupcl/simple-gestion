100a8befadbdbcac02584b578c8ebd1f8c19e59d
Jose Alejandro <dluccajose@gmail.com>
2020-11-11 11:31:40 -0400

    change custom js

--- resources/views/vendor/backpack/crud/fields/commune_shipping_method/custom_script.blade.php
+++ resources/views/vendor/backpack/crud/fields/commune_shipping_method/custom_script.blade.php
@@ -28,7 +28,33 @@
             })
         }
 
+        function showHideTab (tabName, fieldName, classFieldName) {
+            let tab = $(`a[tab_name="${tabName}"]`)
+            let val = $(`input[name="${fieldName}"]`)
+            let checkField =  $(`.${classFieldName}`)
+
+            if (val.val() == 1) {
+                tab.show()
+            } else {
+                tab.hide()
+            }
+            
+            checkField.change(function() {
+                if (val.val() == 1) {
+                    tab.show()
+                } else {
+                    tab.hide()
+                }
+            })
+        }
+
         $(document).ready(function() {
+
+            showHideTab('envio-gratis', 'free_shipping_status', 'free_shipping_checker');
+            showHideTab('tarifa-fija', 'flat_rate_status', 'flat_rate_checker');
+            showHideTab('variable', 'variable_status', 'variable_checker');
+            showHideTab('chilexpress', 'chilexpress_status', 'chilexpress_checker');
+
             removeButtonDel();
             removeButtonAddItem();
             disabledCommuneSelector();
