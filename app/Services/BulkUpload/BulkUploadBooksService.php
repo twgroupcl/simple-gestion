1276f34fb9bbd864d3bfc8f1e9107144dafee66d
Jose Alejandro <dluccajose@gmail.com>
2020-11-13 11:19:41 -0400

    added missin field, message validations and attribute validations

--- app/Services/BulkUpload/BulkUploadBooksService.php
+++ app/Services/BulkUpload/BulkUploadBooksService.php
@@ -8,12 +8,14 @@ use Illuminate\Support\Facades\Validator;
 
 class BulkUploadBooksService {
 
+    const PRODUCT_TYPE = 1;
+
     const ROW_SKU = 1;
     const ROW_NAME = 2;
     const ROW_AUTHOR = 3;
     const ROW_DESCRIPTION= 4;
     const ROW_YEAR = 5;
-    const ROW_BRAND = 6;
+    const ROW_EDITORIAL = 6;
     const ROW_CATEGORY = 7;
     const ROW_LANGUAGE = 8;
     const ROW_NUMBER_PAGES = 9;
@@ -27,10 +29,13 @@ class BulkUploadBooksService {
     const ROW_META_TITLE = 17;
     const ROW_META_KEYWORDS = 18;
     const ROW_META_DESCRIPTION = 19;
+    const ROW_PATH_IMAGE = 20;
 
     private $tableVisibleRows = [
-        'sku' => 'SKU',
+        'sku' => 'ISBN',
         'name' => 'Titulo',
+        'author' => 'Autor',
+        'editorial' => 'Editorial',
         'price' => 'Precio',
         'errors' => 'Errores',
     ];
@@ -51,7 +56,7 @@ class BulkUploadBooksService {
                 'author' => $value[self::ROW_AUTHOR],
                 'description' => $value[self::ROW_DESCRIPTION],
                 'year' => $value[self::ROW_YEAR],
-                'brand' => $value[self::ROW_BRAND],
+                'editorial' => $value[self::ROW_EDITORIAL],
                 'category' => $value[self::ROW_CATEGORY],
                 'language' => $value[self::ROW_LANGUAGE],
                 'pages_number' => $value[self::ROW_NUMBER_PAGES],
@@ -65,11 +70,11 @@ class BulkUploadBooksService {
                 'meta_title' => $value[self::ROW_META_TITLE],
                 'meta_keywords' => $value[self::ROW_META_KEYWORDS],
                 'meta_description' => $value[self::ROW_META_DESCRIPTION],
+                'path_image' => $value[self::ROW_PATH_IMAGE]
             ];
         });
 
         return $this->validate($products);
-        return $products;
     }
 
     public function validate($productsArray)
@@ -79,7 +84,55 @@ class BulkUploadBooksService {
         $productWithErrors = 0;
 
         $rules = [
-            'sku' => 'max:50',
+            'name' => 'required|max:255',
+            'sku' => 'required',
+            'category' => 'required',
+            'author' => 'required', // atributo text
+            'description' => 'required',
+            'year' => 'required|numeric', // atributo text
+            'editorial' => 'required|exists:product_brands,name',
+            'category' => 'required|exists:product_categories,name',
+            'language' => 'required', // atributo quizas select
+            'pages_number' => 'required|numeric', // atributo text
+            'encuadernacion' => 'required', // atributo quizas select
+            'price' => 'required|numeric',
+            'special_price' => 'nullable|numeric',
+            'depth' => 'required|numeric',
+            'width' => 'required|numeric',
+            'height' => 'required|numeric',
+            'weight' => 'required|numeric',
+            'meta_title' => 'required',
+            'meta_keywords' => 'required',
+            'meta_description' => 'required',
+            'path_image' => 'required|ends_with:.jpg,.jpeg,.png',
+        ];
+
+        $messages = [
+            '*.required' => 'El campo :attribute es obligatorio',
+        ];
+
+        $attributes = [
+            'name' => 'Título',
+            'sku' => 'ISBN',
+            'category' => 'Subcategoria',
+            'author' => 'Autores', // atributo text
+            'description' => 'Descripción',
+            'year' => 'Año de edición', // atributo text
+            'editorial' => 'Editorial',
+            'language' => 'Idioma', // atributo quizas select
+            'pages_number' => 'Número de paginas', // atributo text
+            'encuadernacion' => 'Encuadernacion', // atributo quizas select
+            'price' => 'Precio Normal',
+            'special_price' => 'Precio Oferta',
+            'depth' => 'Largo',
+            'width' => 'Ancho',
+            'height' => 'Alto',
+            'weight' => 'Peso',
+            'meta_title' => 'Título para buscadores',
+            'meta_keywords' => 'Palabras clave',
+            'meta_description' => 'Descripción para buscadores',
+            'path_image' => 'Titulo Foto Portada',
+            
         ];
 
         $tmpProducts = $this->removeEmptyRows($productsArray);
@@ -87,7 +140,8 @@ class BulkUploadBooksService {
         foreach ($tmpProducts as $product) {
 
             $product['errors'] = [];
-            $validator = Validator::make($product, $rules/* , $messages, $attributes */);
+
+            $validator = Validator::make($product, $rules , $messages, $attributes);
 
             if ($validator->fails()) {
                 $product['errors'][] = $validator->errors()->all();
@@ -100,7 +154,7 @@ class BulkUploadBooksService {
         }
         
         return [
-            'validate' => false,
+            'validate' => $isValid,
             'products_with_errors' => $productWithErrors,
             'products_array' => $products,
             'table_visible_rows' => $this->tableVisibleRows,
