a2b4ce96d56a005be058c5479a51e36345fc3629
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-13 15:44:36 -0500

    List product container

--- resources/views/livewire/pos/list-products.blade.php
+++ resources/views/livewire/pos/list-products.blade.php
@@ -1,5 +1,7 @@
-<div>
-    @foreach ($products as $product)
-        @livewire('pos.product', ['product' => $product], key($product->id))
-    @endforeach
+<div class="container">
+    <div class="row">
+        @foreach ($products as $product)
+            @livewire('pos.product', ['product' => $product], key($product->id))
+        @endforeach
+    </div>
 </div>
