047739473ba390ac16ff34ea69a9416d3eecd813
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-13 15:42:31 -0500

    Show product image, name and price in component

--- resources/views/livewire/pos/product.blade.php
+++ resources/views/livewire/pos/product.blade.php
@@ -1,3 +1,16 @@
-<div>
-    {{ $product->name }}
+<div class="col-md-4 mb-3">
+    <div class="card h-100">
+        <div class="d-flex justify-content-end position-absolute w-100">
+            <div class="label-sale">
+                <span class="badge badge-success">+</span>
+            </div>
+        </div>
+        <a href="#" class="p-3">
+        <img src="{{ url($product->getFirstImagePath()) }}" class="card-img-top" alt="Product">
+        </a>
+        <div class="card-body">
+            <p class="text-center w-100"> {{ $product->name }}</p>
+            <p class="h5 text-center w-100">{{ currencyFormat($product->real_price, 'CLP', true) }}</p>
+        </div>
+    </div>
 </div>
