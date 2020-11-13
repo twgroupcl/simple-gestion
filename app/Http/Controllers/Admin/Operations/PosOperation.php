22f4cafca5f6075b366157ed1f0eb203bfc83466
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-13 11:25:45 -0500

    Create Pos operation and crud controller

---
+++ app/Http/Controllers/Admin/Operations/PosOperation.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Operations;
+
+use Illuminate\Support\Facades\Route;
+
+trait PosOperation
+{
+    /**
+     * Define which routes are needed for this operation.
+     *
+     * @param string $segment    Name of the current entity (singular). Used as first URL segment.
+     * @param string $routeName  Prefix of the route name.
+     * @param string $controller Name of the current CrudController.
+     */
+    protected function setupPosRoutes($segment, $routeName, $controller)
+    {
+        Route::get($segment.'/pos', [
+            'as'        => $routeName.'.pos',
+            'uses'      => $controller.'@pos',
+            'operation' => 'pos',
+        ]);
+    }
+
+    /**
+     * Add the default settings, buttons, etc that this operation needs.
+     */
+    protected function setupPosDefaults()
+    {
+        $this->crud->allowAccess('pos');
+
+        $this->crud->operation('pos', function () {
+            $this->crud->loadDefaultOperationSettingsFromConfig();
+        });
+
+        $this->crud->operation('list', function () {
+            // $this->crud->addButton('top', 'pos', 'view', 'crud::buttons.pos');
+            // $this->crud->addButton('line', 'pos', 'view', 'crud::buttons.pos');
+        });
+    }
+
+    /**
+     * Show the view for performing the operation.
+     *
+     * @return Response
+     */
+    public function pos()
+    {
+        $this->crud->hasAccessOrFail('pos');
+
+        // prepare the fields you need to show
+        $this->data['crud'] = $this->crud;
+        $this->data['title'] = $this->crud->getTitle() ?? 'pos '.$this->crud->entity_name;
+
+        // load the view
+        return view("crud::operations.pos", $this->data);
+    }
+}
