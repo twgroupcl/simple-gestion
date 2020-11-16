9ce25a6f8a8e7265a58a326a4638859e61d2de03
Jose Alejandro <dluccajose@gmail.com>
2020-11-15 21:11:35 -0400

    added bulk upload store view

---
+++ resources/views/admin/products/bulk-upload-store.blade.php
@@ -0,0 +1,37 @@
+@extends(backpack_view('blank'))
+
+@section('content')
+    <div class="row mt-3">
+        <div class="col">
+            <h3>Subir productos masivamente</h3>
+        </div>
+    </div>
+
+    @if ( !empty($error))
+        <div class="alert alert-success pb-0">
+            <ul class="list-unstyled">
+                    <li><i class="la la-info-circle"></i> {{ session('error') }}</li>
+            </ul>
+        </div>    
+    @endif
+
+    @if ( !empty($success))
+        <div class="alert alert-success pb-0">
+            <ul class="list-unstyled">
+                    <li><i class="la la-info-circle"></i> {{ $success }}</li>
+            </ul>
+        </div>    
+    @endif
+
+    <div class="btn-group" role="group">
+        <a href="{{ backpack_url('product') }}">
+        <button type="submit" class="btn btn-success">
+            <span class="la la-box" role="presentation" aria-hidden="true"></span> &nbsp;
+            <span>Ir a la pagina de Productos</span>
+        </button>
+        </a>
+    </div>
+
+    <a href="{{ route('products.bulk-upload') }}" class="btn btn-default"><span class="la la-angle-left"></span> &nbsp;Volver y cargar otro archivo</a>
+
+@endsection
