474ba082fef17c541c9d069eb9fae97b1d876946
Jose Alejandro <dluccajose@gmail.com>
2020-11-17 13:02:58 -0400

    added new rules to commune shipping method

--- app/Http/Requests/CommuneShippingMethodRequest.php
+++ app/Http/Requests/CommuneShippingMethodRequest.php
@@ -3,11 +3,23 @@
 namespace App\Http\Requests;
 
 use App\Http\Requests\Request;
+use App\Rules\NumericCommaRule;
 use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+
 
 class CommuneShippingMethodRequest extends FormRequest
 {
+
+    private $prepareData = [
+        'free_shipping',
+        'picking',
+        'variable',
+        'chilexpress',
+        'flat_rate',
+    ];
+
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -26,6 +38,8 @@ class CommuneShippingMethodRequest extends FormRequest
      */
     public function rules()
     {
+        $numericCommaRule = new NumericCommaRule(0);
+        
         return [
             'commune_id' => [
                 'required_if:is_global,0',
@@ -40,6 +54,31 @@ class CommuneShippingMethodRequest extends FormRequest
                 }),
             ],
             'shipping_methods_validation' => 'required',
+
+            'variable_validation.*.fallback_price' => 'required_if:variable_status,1|numeric|min:0.1',
+
+            'variable_validation.*.table_prices' => 'required_if:variable_status,1',
+            'variable_validation_table_prices.*.min_weight' => ['required_if:variable_status,1', $numericCommaRule],
+            'variable_validation_table_prices.*.max_weight' => ['required_if:variable_status,1', $numericCommaRule],
+            'variable_validation_table_prices.*.final_price' => ['required_if:variable_status,1', $numericCommaRule],
+
+            /* 'variable_validation.*.table_prices' => [
+                'required_if:variable_status,1',
+                function ($attribute, $value, $fail) {
+                    $tablePrices = json_decode($value, true);
+
+                    $fieldGroupValidator = Validator::make($tablePrices, 
+                    [
+                        '*.min_price' => 'required|numeric|min:0'
+                    ]);
+
+                    if ($fieldGroupValidator->fails()) {
+                        $this->message = 'Hay un error en tu variacion. '  . $fieldGroupValidator->errors()->first();
+                        return $fail('er');
+                    }
+                    
+                }
+            ], */
 
         ];
     }
@@ -87,6 +126,34 @@ class CommuneShippingMethodRequest extends FormRequest
             $this->merge([
                 'shipping_methods_validation' => $shippingMethods,
             ]); 
+        }
+
+        foreach ($this->prepareData as $attrName) {
+            
+            if (empty($this->$attrName)) {
+                return;
+            }
+
+            if ($this->{$attrName . '_status'}) {
+                $validation = json_decode($this->$attrName);
+                $forValidation = [];
+
+                foreach ($validation as $attrs) {
+                    $forValidation[] = (array) $attrs;
+                }
+                
+                if (isset($attrs->table_prices)) {
+                    $this->merge([
+                        'variable_validation_table_prices' => !empty($attrs->table_prices)
+                            ? json_decode($attrs->table_prices, true)
+                            : [],
+                    ]);
+                }
+
+                $this->merge([
+                    $attrName.'_validation' => $forValidation,
+                ]);
+            }
         }
     }
 }
