052446341dc3c4809dd9e4cae2cd0408ce2f8537
Jose Alejandro <dluccajose@gmail.com>
2020-11-18 12:08:08 -0400

    change produto for libro in sidebar menu and bulk upload button

--- resources/views/vendor/backpack/crud/buttons/product/bulk-upload.blade.php
+++ resources/views/vendor/backpack/crud/buttons/product/bulk-upload.blade.php
@@ -1,3 +1,3 @@
 @if ($crud->hasAccess('create'))
-	<a href="{{ route('products.bulk-upload') }}" class="btn btn-primary" data-style="zoom-in"><span class="ladda-label"><i class="la la-plus"></i>Subir productos masivamente</span></a>
+	<a href="{{ route('products.bulk-upload') }}" class="btn btn-primary" data-style="zoom-in"><span class="ladda-label"><i class="la la-plus"></i>Subir {{ $crud->entity_name_plural }} masivamente</span></a>
 @endif
