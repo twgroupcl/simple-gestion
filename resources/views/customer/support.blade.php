6067cc4da49a7beb5d5f38c602a5db268795ba79
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-19 13:25:41 -0500

    Fix and refactor form

--- resources/views/customer/support.blade.php
+++ resources/views/customer/support.blade.php
@@ -6,84 +6,92 @@
         <div class="col-md-8">
             <h2 class="h4 mb-5">Servicio al cliente</h2>
 
-            <form method="POST"">
+            <form method="POST" action="{{ route('customer.support.create') }}">
                 @csrf
                 <div class="row">
                     <div class="col-sm-6">
                         <div class="input-group">
-                            <label for="contact-type" class="control-label">Tipo de contacto <span class="text-danger">*</span></label><br>
+                            <label for="contact_type" class="control-label">Tipo de contacto <span class="text-danger">*</span></label><br>
                             <div class="w-100">
-                                <select class="form-control" id="contact-type">
-                                    <option selected>Seleccione una opción</option>
-                                    <option value="1">Consulta</option>
-                                    <option value="2">Reclamo</option>
-                                    <option value="3">Sugerencia</option>
+                                <select class="form-control" name="contact_type" id="contact_type">
+                                    <option selected>Seleccione una opción...</option>
+                                    <option value="1">1. Consulta</option>
+                                    <option value="2">2. Reclamo</option>
+                                    <option value="3">3. Sugerencia</option>
                                 </select>
                             </div>
+                            @error('contact_type')
+                            <small class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </small>
+                            @enderror
                           </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="first_name">Asunto <span class="text-danger">*</span></label>
-                            <input class="form-control @error('first_name') is-invalid @enderror" type="text" name="first_name" id="first_name" placeholder="Escribe aquí tu nombre" value="{{ old('first_name') }}" required>
-                            <div class="invalid-feedback">Por favor ingresa el asunto.</div>
-                            @error('first_name')
-                            <span class="invalid-feedback" role="alert">
+                            <label for="subject">Asunto <span class="text-danger">*</span></label>
+                            <input class="form-control @error('subject') is-invalid @enderror" type="text" name="subject" id="subject" placeholder="Escribe aquí el asunto" value="{{ old('subject') }}" >
+                            @error('subject')
+                            <small class="text-danger" role="alert">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </small>
                             @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="first_name">Nombre <span class="text-danger">*</span></label>
-                            <input class="form-control @error('first_name') is-invalid @enderror" type="text" name="first_name" id="first_name" placeholder="Escribe aquí tu nombre" value="{{ old('first_name') }}" required>
-                            <div class="invalid-feedback">Por favor ingresa tu nombre.</div>
-                            @error('first_name')
-                            <span class="invalid-feedback" role="alert">
+                            <label for="name">Nombre <span class="text-danger">*</span></label>
+                            <input class="form-control @error('name') is-invalid @enderror" type="text" name="name" id="name" placeholder="Escribe aquí tu nombre" value="{{ old('name') }}" >
+                            @error('name')
+                            <small class="text-danger" role="alert">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </small>
                             @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
                             <label for="email">E-mail <span class="text-danger">*</span></label>
-                            <input class="form-control @error('email') is-invalid @enderror" type="email" name="email" id="email" placeholder="Escribe aquí tu email" value="{{ old('email') }}" required>
-                            <div class="invalid-feedback">Por favor ingresa un email válido.</div>
+                            <input class="form-control @error('email') is-invalid @enderror" type="email" name="email" id="email" placeholder="Escribe aquí tu email" value="{{ old('email') }}" >
                             @error('email')
-                            <span class="invalid-feedback" role="alert">
+                            <small class="text-danger" role="alert">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </small>
                             @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="password">Teléfono <span class="text-danger">*</span></label>
-                            <div class="password-toggle">
-                                <input class="form-control" type="text" name="phone" id="phone" required>
-                            </div>
+                            <label for="phone">Teléfono <span class="text-danger">*</span></label>
+                            <input class="form-control @error('phone') is-invalid @enderror" type="text" name="phone" id="phone" value="{{ old('phone') }}" >
                             @error('phone')
-                            <span class="invalid-feedback" role="alert">
+                            <small class="text-danger" role="alert">
                                 <strong>{{ $message }}</strong>
-                            </span>
+                            </small>
                             @enderror
                         </div>
                     </div>
                     <div class="col-sm-6">
                         <div class="form-group">
-                            <label for="password_confirmation">N° de Orden <span class="text-danger">*</span></label>
-                            <div class="password-toggle">
-                                <input class="form-control @error('order_number') is-invalid @enderror" type="text" name="order_number" id="order_number" required>
-                            </div>
+                            <label for="order_id">N° de Orden</label>
+                            <input class="form-control @error('order_id') is-invalid @enderror" type="number" name="order_id" id="order_id" value="{{ old('order_id') }}" >
+                            @error('order_id')
+                            <small class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </small>
+                            @enderror
                         </div>
                     </div>
                     <div class="col-sm-12">
                         <div class="form-group">
-                            <label for="exampleFormControlTextarea1">Detalle <span class="text-danger">*</span></label>
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                          </div>
+                            <label for="details">Detalle <span class="text-danger">*</span></label>
+                            <textarea class="form-control @error('details') is-invalid @enderror"" name="details" id="details" rows="3">{{ old('details') }}</textarea>
+                            @error('details')
+                            <small class="text-danger" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </small>
+                            @enderror
+                        </div>
                     </div>
                 </div>
                 <div class="text-right">
