c24f231379b6520285c3a5471f82cba65bbb08e2
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-11-19 08:49:28 -0500

    Create customer support migrations

---
+++ database/migrations/2020_11_19_101332_create_customer_support_history_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomerSupportHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_support_history', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('issue_id');
+            $table->text('note');
+            $table->timestamps();
+        });
+
+        Schema::table('customer_support_history', function (Blueprint $table) {
+            $table->foreign('issue_id')->references('id')->on('customer_support');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer_support_history');
+    }
+}
