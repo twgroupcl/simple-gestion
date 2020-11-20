1bc3332eb3e833939b4065307426e5362357a17b
Julian Murphy <julian.murphy@twgroup.cl>
2020-11-20 18:02:03 -0300

    disable buttons

--- resources/views/invoice/manage_invoice/delete_temporary_document.blade.php
+++ resources/views/invoice/manage_invoice/delete_temporary_document.blade.php
@@ -1,3 +1,11 @@
-<a href="{{ url('admin/invoice/'.$invoice->id.'/delete-temporary-document') }} " class="btn btn-sm btn-danger">
+<a href="{{ url('admin/invoice/'.$invoice->id.'/delete-temporary-document') }}" class="btnDisable btn btn-sm btn-danger">
     <i class="la la-send"></i> Eliminar documento temporal
 </a>
+@push('after_scripts')
+<script>
+    console.log($('.btnDisable'))
+    $('.btn').on('onclick', function() {
+        this.parent().find('a').attr('disable','disable')
+    })
+</script>
+@endpush
