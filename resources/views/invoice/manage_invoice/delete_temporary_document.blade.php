c4221cdbe81210c1042c1c1cf9e6a7b090585f59
Julian Murphy <julian.murphy@twgroup.cl>
2020-11-20 18:31:16 -0300

    clear

--- resources/views/invoice/manage_invoice/delete_temporary_document.blade.php
+++ resources/views/invoice/manage_invoice/delete_temporary_document.blade.php
@@ -1,11 +1,3 @@
 <a href="{{ url('admin/invoice/'.$invoice->id.'/delete-temporary-document') }}" class="btnDisable btn btn-sm btn-danger">
     <i class="la la-send"></i> Eliminar documento temporal
 </a>
-@push('after_scripts')
-<script>
-    console.log($('.btnDisable'))
-    $('.btn').on('onclick', function() {
-        this.parent().find('a').attr('disable','disable')
-    })
-</script>
-@endpush
