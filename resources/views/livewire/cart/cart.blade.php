51f6109e2f5aa3afca8360cc12b55629eab7a8e0
Julio Barragan <jcbarragan1994@gmail.com>
2020-11-21 16:27:00 -0400

    changes

--- resources/views/livewire/cart/cart.blade.php
+++ resources/views/livewire/cart/cart.blade.php
@@ -3,8 +3,10 @@
 
     @livewire('cart.cart-counter', ['count' => $cart->items_count])
 
-    <a class="navbar-tool-text" href="{{ $cursor === 'auto' ? route('shopping-cart') : '#' }}"  style="cursor:{{ $cursor === 'auto' ? 'pointer' : $cursor }};">
-        <small>Carro</small>{{ currencyFormat($subtotal, defaultCurrency(), true) }}
+    <a class="" href="{{ $cursor === 'auto' ? route('shopping-cart') : '#' }}"  style="cursor:{{ $cursor === 'auto' ? 'pointer' : $cursor }};">
+        <!--
+            <small>Carro</small>{{ currencyFormat($subtotal, defaultCurrency(), true) }}
+        -->    
     </a>
     @if ($cursor == 'auto')
         <!-- Cart dropdown-->
