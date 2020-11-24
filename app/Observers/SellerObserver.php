4029792809717a85d128accfe02579b82eb773ca
Julian Murphy <julian.murphy@twgroup.cl>
2020-11-24 19:25:54 -0300

    merge commit 40172de

--- app/Observers/SellerObserver.php
+++ app/Observers/SellerObserver.php
@@ -6,12 +6,12 @@ use App\Mail\NotificationSuscription;
 use App\Mail\SellerChangeStatus;
 use App\Models\BranchUser;
 use App\Models\CompanyUser;
+use App\Models\Currency;
 use App\Models\PaymentMethodSeller;
 use App\Models\Seller;
 use App\Models\SellerAddress;
 use App\Models\PlanSuscription;
 use App\Models\ShippingMethodSeller;
-use App\Models\Plans;
 use App\User;
 use Backpack\Settings\app\Models\Setting;
 use Illuminate\Support\Facades\Mail;
@@ -131,23 +131,39 @@ class SellerObserver
 
     public function syncSuscription(Seller $seller)
     {
-        $suscription_data = is_array($seller->suscription_data)
-        ? $seller->suscription_data
-        : json_decode($seller->suscription_data, true);
-     
-        $user = User::find($seller->user->id);
-        $planName = Plans::find($suscription_data['id_plan']);
-        $plan = app('rinvex.subscriptions.plan')->find($suscription_data['id_plan']);
-        $newSuscription = $user->newSubscription('plan', $plan);
+        // $seller->subscriptions()->delete();
+
+        $subscription_data = is_array($seller->subscription_data)
+        ? $seller->subscription_data
+        : json_decode($seller->subscription_data, true);
+
+        // if (!empty($subscription_data['plan_subscription_id'])) {
+        //     $newplansubscription = new PlanSubscriptionSeller($subscription_data);
+        //     $seller->subscriptions()->save($newplansubscription);
+        // }
+       // if (!empty($subscription_data['plan_subscription_id'])) {
+            $user = User::find($seller->user->id);
+            // $user->subscription('main')->usage()->delete();
+            $plan = app('rinvex.subscriptions.plan')->find($subscription_data['plan_id']);
+            $newSubscription = $user->newSubscription('plan', $plan);
+            $plan = Plans::where('id', $newSubscription->plan_id)->first();
+            if ($plan->price > 0) {
+                return redirect()->route('payment.subscription', ['id' => $newSubscription->id])->send();
+            }
+       // }else{
+            $plan = Plans::where('id', $subscription_data['plan_id'])->first();
+      //  }
+        $currency = Currency::where('id',$plan->currency)->first();
 
         $dataEmail = [
             'seller' => $seller->name,
-            'plan' => $planName->name,
+            'plan' => $plan->name,
             'price' => $plan->price,
-            'currency' => $plan->currency,
-            'start_date' => $suscription_data['starts_at'],
-            'end_date' => $suscription_data['ends_at']
+            'currency' => $currency->code,
+            'start_date' => $subscription_data['starts_at'],
+            'end_date' => $subscription_data['ends_at']
         ];
+
 
         $emailsAdministrator = explode(';', Setting::get('administrator_email'));
         array_push($emailsAdministrator, $seller->email);
--- database/seeds/PlansTableSeeder.php
+++ database/seeds/PlansTableSeeder.php
@@ -4,52 +4,96 @@ use Illuminate\Database\Seeder;
 
 class PlansTableSeeder extends Seeder
 {
+
     /**
-     * Run the database seeds.
+     * Auto generated seed file
      *
      * @return void
      */
     public function run()
     {
+        
+
         \DB::table('plans')->delete();
         
         \DB::table('plans')->insert(array (
             0 => 
             array (
                 'id' => 1,
-                'slug' => 'plan-semanal',
-                'name' => 'Plan Semanal',
-                'price' => 50,
-                'currency' => 'USD',
-                'invoice_interval' => 'week',
-                'created_at' => '2020-10-14 17:22:07',
-                'updated_at' => '2020-10-14 17:22:07',
+                'slug' => 'socios-crcp',
+                'name' => 'SOCIOS CRCP',
+                'description' => NULL,
+                'is_active' => 1,
+                'price' => '0.00',
+                'signup_fee' => '0.00',
+                'currency' => '1',
+                'trial_period' => 0,
+                'trial_interval' => 'day',
+                'invoice_period' => 0,
+                'invoice_interval' => 'month',
+                'grace_period' => 0,
+                'grace_interval' => 'day',
+                'prorate_day' => 0,
+                'prorate_period' => 0,
+                'prorate_extend_due' => 0,
+                'active_subscribers_limit' => NULL,
+                'sort_order' => 0,
+                'created_at' => '2020-11-09 16:58:03',
+                'updated_at' => '2020-11-09 16:58:03',
                 'deleted_at' => NULL,
             ),
             1 => 
             array (
                 'id' => 2,
-                'slug' => 'plan-mensual',
-                'name' => 'Plan Mensual',
-                'price' => 100,
-                'currency' => 'USD',
+                'slug' => 'no-socios',
+                'name' => 'NO SOCIOS',
+                'description' => NULL,
+                'is_active' => 1,
+                'price' => '25000.00',
+                'signup_fee' => '0.00',
+                'currency' => '1',
+                'trial_period' => 0,
+                'trial_interval' => 'day',
+                'invoice_period' => 0,
                 'invoice_interval' => 'month',
-                'created_at' => '2020-10-14 17:22:07',
-                'updated_at' => '2020-10-14 17:22:07',
+                'grace_period' => 0,
+                'grace_interval' => 'day',
+                'prorate_day' => 0,
+                'prorate_period' => 0,
+                'prorate_extend_due' => 0,
+                'active_subscribers_limit' => NULL,
+                'sort_order' => 0,
+                'created_at' => '2020-11-09 16:58:16',
+                'updated_at' => '2020-11-09 16:58:16',
                 'deleted_at' => NULL,
             ),
             2 => 
             array (
                 'id' => 3,
-                'slug' => 'plan-mensual-especial',
-                'name' => 'Plan Mensual Especial',
-                'price' => 150,
-                'currency' => 'USD',
+                'slug' => 'emprendedores',
+                'name' => 'EMPRENDEDORES',
+                'description' => NULL,
+                'is_active' => 1,
+                'price' => '12500.00',
+                'signup_fee' => '0.00',
+                'currency' => '1',
+                'trial_period' => 0,
+                'trial_interval' => 'day',
+                'invoice_period' => 0,
                 'invoice_interval' => 'month',
-                'created_at' => '2020-10-14 17:22:07',
-                'updated_at' => '2020-10-14 17:22:07',
+                'grace_period' => 0,
+                'grace_interval' => 'day',
+                'prorate_day' => 0,
+                'prorate_period' => 0,
+                'prorate_extend_due' => 0,
+                'active_subscribers_limit' => NULL,
+                'sort_order' => 0,
+                'created_at' => '2020-11-09 16:58:30',
+                'updated_at' => '2020-11-09 16:58:30',
                 'deleted_at' => NULL,
             ),
         ));
+        
+        
     }
 }
