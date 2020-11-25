6e5f10ed13b912ed5113846f08d17728ee053709
Julio Barragan <jcbarragan1994@gmail.com>
2020-11-25 18:18:58 -0400

    relation plan

--- app/Models/PlanSubscription.php
+++ app/Models/PlanSubscription.php
@@ -8,4 +8,9 @@ class PlanSubscription extends Model
 {
     protected $table = 'plan_subscriptions';    
     protected $primaryKey = 'slug';
+
+    public function plan()
+    {
+        return $this->belongsTo(Plans::class);
+    }
 }
