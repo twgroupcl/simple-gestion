d6d6469ceaa51ed1a610cbcd103e8c789e925dc7
Jose Alejandro <dluccajose@gmail.com>
2020-12-02 10:59:28 -0400

    Added accesors and crud trait

--- app/Models/CustomerAttendance.php
+++ app/Models/CustomerAttendance.php
@@ -2,12 +2,21 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class CustomerAttendance extends Model
 {
+    use CrudTrait;
+
     protected $table = 'customer_attendances';
     protected $guarded = ['id'];
+
+    /*
+    |--------------------------------------------------------------------------
+    | GLOBAL VARIABLES
+    |--------------------------------------------------------------------------
+    */
 
     const CHECK_IN = 1;
     const CHECK_OUT = 2;
@@ -33,5 +42,29 @@ class CustomerAttendance extends Model
     public function branch()
     {
         return $this->belongsTo(Branch::class);
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESSORS
+    |--------------------------------------------------------------------------
+    */
+
+    public function getDateOnlyAttribute()
+    {
+        return $this->attendance_time;
+    }
+
+    public function getEntryTypeAccesorAttribute()
+    {
+        switch ($this->entry_type) {
+            case self::CHECK_IN:
+                return 'Check In';
+                break;
+            
+            case self::CHECK_OUT:
+                return 'Check Out';
+                break;
+        }
     }
 }
