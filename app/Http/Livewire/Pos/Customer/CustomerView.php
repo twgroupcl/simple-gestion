7d68722e00687aa38f36285fed058383c875f62b
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-12-02 14:08:05 -0500

    Load customer also on load page

--- app/Http/Livewire/Pos/Customer/CustomerView.php
+++ app/Http/Livewire/Pos/Customer/CustomerView.php
@@ -9,15 +9,21 @@ use Livewire\Component;
 class CustomerView extends Component
 {
     public $view;
-    public $search;
+    public $search = '';
     private $customers = [];
 
     public function render()
     {
+        $this->filter();
         return view('livewire.pos.customer.customer-view', ['customers' => $this->customers]);
     }
 
     public function updatedSearch()
+    {
+        $this->filter();
+    }
+
+    public function filter()
     {
         $this->customers = Customer::where(DB::raw("CONCAT(`first_name`, ' ', `last_name`)"), 'LIKE', "%{$this->search}%")
                                     ->orWhere('uid', 'LIKE', "%{$this->search}%")
