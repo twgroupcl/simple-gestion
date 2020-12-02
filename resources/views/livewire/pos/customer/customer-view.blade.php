2037d7d0488be9dad1dff0803da959006806ddea
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-12-02 14:58:29 -0500

    Fix list

--- resources/views/livewire/pos/customer/customer-view.blade.php
+++ resources/views/livewire/pos/customer/customer-view.blade.php
@@ -1,9 +1,18 @@
-<div class="container {{ $view !== 'selectCustomer' ? 'd-none' : '' }})">
-    <label for="searchCustomer">Buscar cliente</label>
-    <input wire:model="search" class="form-control" type="text" id="searchCustomer" placeholder="Buscar" aria-label="Buscar">
-    <ul class="list-group">
-        @foreach ($customers as $customer)
-            <li class="list-group-item">{{ $customer->first_name }}</li>
-        @endforeach
-    </ul>
+<div class="row {{ $view !== 'selectCustomer' ? 'd-none' : '' }})">
+    <div class="col-md-6">
+        <label for="searchCustomer">Buscar cliente</label>
+        <input wire:model="search" class="form-control" type="text" id="searchCustomer" placeholder="Buscar" aria-label="Buscar">
+        <ul class="list-group list-group-flush">
+            @foreach ($customers as $customer)
+                <div class="bg-light border-right">
+                    <div class="list-group list-group-flush">
+                        <a class="list-group-item list-group-item-action bg-white">
+                            <p class="text-info my-0">{{ $customer->first_name }}</p>
+                            <p class="my-0">{{ $customer->uid }}</p>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </ul>
+    </div>
 </div>
