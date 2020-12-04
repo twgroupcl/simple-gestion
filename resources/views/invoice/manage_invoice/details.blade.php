0282dd7974e014225adcd74b73134c9a88649e85
Julian Murphy <julian.murphy@twgroup.cl>
2020-12-04 19:09:08 -0300

    style buttons and titles

--- resources/views/invoice/manage_invoice/details.blade.php
+++ resources/views/invoice/manage_invoice/details.blade.php
@@ -1,19 +1,31 @@
-
 <div class="row">
-    <div class="col-md-3">
-        <p><strong>Nombre: </strong>
-            {{$invoice->first_name}}
-        </p>
+    <h3>Detalles de la factura:</h3>
+</div>
+<div class="container-fluid animated fadeIn card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-3">
+                <p><strong>Nombre: </strong>
+                    {{$invoice->first_name}}
+                </p>
+            </div>
+            <div class="col-md-3">
+                <p><strong>Total: </strong>
+                    {{$invoice->total}}
+                </p>
+            </div>
+            <div class="col-md-3">
+                <p><strong>Fecha: </strong>
+                {{$invoice->invoice_date}}
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <p><strong>Código de documento: </strong>
+                    {{$invoice->dte_code ?? "S/código"}}
+                </p>
+            </div>
+        </div>
     </div>
-    <div class="col-md-3">
-        <p><strong>Total: </strong>
-            {{$invoice->total}}
-        </p>
-    </div>
-    <div class="col-md-3">
-        <p><strong>Código de documento: </strong>
-            {{$invoice->dte_code}}
-        </p>
-    </div>
-
 </div>
