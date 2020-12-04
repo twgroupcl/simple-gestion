404720dbca1dbc3ef75373cbc6a72c45fda1fa75
Julian Murphy <julian.murphy@twgroup.cl>
2020-12-04 19:09:08 -0300

    style buttons and titles

--- resources/views/invoice/manage_invoice/index.blade.php
+++ resources/views/invoice/manage_invoice/index.blade.php
@@ -12,71 +12,14 @@
 @php
     Widget::add([
         'type' => 'view',
-        'view' => 'invoice.manage_invoice.details'
+        'view' => 'invoice.manage_invoice.details',
     ]);
 @endphp
 
-@can('canCreateTempDocument', $invoice)
 @php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.create_temporary_document'
-    ]);
+    Widget::add([ 'type' => 'column_break' ]);
 @endphp
-@endcan
 
-@can('doEdit', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.go_to_edit',
-    ]);
-@endphp
-@endcan
+@include('invoice.manage_invoice.actions')
 
-@can('doDownloadTemporalPDF', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.get_temporal_pdf',
-    ]);
-@endphp
-@endcan
-
-@can('doDownloadRealPDF', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.get_real_pdf',
-    ]);
-@endphp
-@endcan
-
-{{-- status invoice --}}
-@can('doDeleteDocument', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.delete_temporary_document',
-    ]);
-@endphp
-@endcan
-
-@can('doShowTempDocument', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.create_real_document',
-    ]);
-@endphp
-@endcan
-
-@can('doDownloadRealPDF', $invoice)
-@php
-    Widget::add([
-        'type' => 'view',
-        'view' => 'invoice.manage_invoice.issue_credit_note',
-    ]);
-@endphp
-@endcan    
 @endsection
