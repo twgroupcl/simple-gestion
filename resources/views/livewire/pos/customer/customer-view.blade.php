15868e87c335d0492d0acb9f8e829b35ae75e51e
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-12-04 10:08:11 -0500

    fix: Mix customer view with form in same component

--- resources/views/livewire/pos/customer/customer-view.blade.php
+++ resources/views/livewire/pos/customer/customer-view.blade.php
@@ -58,7 +58,42 @@
         aria-hidden="true"
         >
             <div class="modal-dialog" role="document">
-                <livewire:pos.customer.create-customer>
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button
+                        type="button"
+                        class="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="col-lg-12 pt-4 pt-lg-0">
+                            <div class="product-details ml-auto pb-3">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                            Cerrar
+                        </button>
+                    </div>
+                </div>
             </div>
     </div>
 </div>
+
+@push('after_scripts')
+<script>
+    const showCustomerModal = () => {
+        $('#showCustomerModal').appendTo("body").modal('show');
+    }
+
+    $("#showCustomerModal").on('hidden.bs.modal', function () {
+        $('#showCustomerModal').appendTo("body").modal('hide');
+    });
+</script>
+@endpush
