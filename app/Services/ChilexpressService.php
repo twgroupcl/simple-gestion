d79f6616cdda339a95d2b1ad058524b37eaf6c72
Jorge Castro <jorge.castro@twgroup.cl>
2020-12-04 21:46:55 -0300

    Change endpoints Chilexpress

--- app/Services/ChilexpressService.php
+++ app/Services/ChilexpressService.php
@@ -7,7 +7,7 @@ use GuzzleHttp\Client;
 class ChilexpressService
 {
 	private $client;
-	private $sandboxUrl = 'https://testservices.wschilexpress.com/rating/api/v1.0/rates/courier';
+	private $sandboxUrl = 'https://qaservices.wschilexpress.com/rating/api/v1.0/rates/courier';
 
 	public function __construct()
 	{
@@ -22,7 +22,7 @@ class ChilexpressService
 				'headers' => [
 					'Content-Type' => 'application/json',
 					'Cache-Control' => 'no-cache',
-					'Ocp-Apim-Subscription-Key' => 'f767358f953f4feb8020e44deee43b6b',
+					'Ocp-Apim-Subscription-Key' => '058a8ce59e4b44888c35134e0b7a6245',
 				],
 			]);
 		} catch (\GuzzleHttp\Exception\ClientException $e) {
@@ -34,14 +34,37 @@ class ChilexpressService
 
 	public function coverage($regionCode)
 	{
-		$response = $this->client->get('https://testservices.wschilexpress.com/georeference/api/v1.0/coverage-areas?RegionCode='.$regionCode.'&type=0');
-
+		//$response = $this->client->get('https://qaservices.wschilexpress.com/georeference/api/v1.0/coverage-areas?RegionCode='.$regionCode.'&type=0');
+		try {
+			$response = $this->client->get('https://qaservices.wschilexpress.com/georeference/api/v1.0/coverage-areas?RegionCode='.$regionCode.'&type=0', [
+				'headers' => [
+					'Content-Type' => 'application/json',
+					'Cache-Control' => 'no-cache',
+					'Ocp-Apim-Subscription-Key' => 'd89a06bd90db4fd8a32c5d66d1fbd5fc',
+				],
+			]);
+		} catch (\GuzzleHttp\Exception\ClientException $e) {
+            dd($e->getResponse()->getBody()->getContents());
+		}
+		
 		return json_decode($response->getBody()->getContents());
     }
 
     public function getRegions()
 	{
-		$response = $this->client->get('https://testservices.wschilexpress.com/georeference/api/v1.0/regions');
+		//$response = $this->client->get('https://qaservices.wschilexpress.com/georeference/api/v1.0/regions');
+		try {
+			$response = $this->client->get('https://qaservices.wschilexpress.com/georeference/api/v1.0/regions', [
+				'headers' => [
+					'Content-Type' => 'application/json',
+					'Cache-Control' => 'no-cache',
+					'Ocp-Apim-Subscription-Key' => 'd89a06bd90db4fd8a32c5d66d1fbd5fc',
+				],
+			]);
+		} catch (\GuzzleHttp\Exception\ClientException $e) {
+            dd($e->getResponse()->getBody()->getContents());
+		}
+
 		return json_decode($response->getBody()->getContents());
 	}
 }
