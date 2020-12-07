a727a81ce54310ec930870dee0c96424576f7c72
Roberto Orellana <roberto.orellana@twgroup.cl>
2020-12-07 15:32:07 -0500

    Add open/closed accesor

--- app/Models/SalesBox.php
+++ app/Models/SalesBox.php
@@ -53,6 +53,16 @@ class SalesBox extends Model
     |--------------------------------------------------------------------------
     */
 
+    public function getIsOpenedAttribute()
+    {
+        return ($this->open_at !== null) && ($this->closed_at === null);
+    }
+
+    public function getIsClosedAttribute()
+    {
+        return ($this->open_at !== null) && ($this->closed_at !== null);
+    }
+
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
