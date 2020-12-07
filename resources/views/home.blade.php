06113f9ab8d499f5540175e190f871a482d2db47
Julio Barragan <jcbarragan1994@gmail.com>
2020-12-07 19:47:04 -0400

    cortina mobile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,10 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css') }}">
+
+
         <title>Filsa Virtual</title>
         <style>
             body,
@@ -23,10 +27,19 @@
             .bgimg-1 {
                 background-image: url('img/filsa/cortina-web.jpg');
                 height: 100%;
+
+            }
+            .bgimg-2 {
+                background-image: url('img/filsa/cortina-mobile.jpg');
+                height: 100%;
             }
         </style>
+
     </head>
     <body>
-        <div class="bgimg-1"></div>
+        <div class="d-none d-lg-block d-md-block d-sm-block bgimg-1">
+        </div>
+        <div class="d-block d-sm-none bgimg-2">
+        </div>
     </body>
 </html>
