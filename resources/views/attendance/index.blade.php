61bcb85af991101ed28f785fbb99b9cbea512a0a
Jose Alejandro <dluccajose@gmail.com>
2020-12-07 11:13:49 -0400

    change attendance layout

--- resources/views/attendance/index.blade.php
+++ resources/views/attendance/index.blade.php
@@ -1,120 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('attendance.layout.base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrar asistencia - {{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css') }}">
-    <style>
-        body {
-            background-color: #fbf9f4;
-        }
+@section('title')
+    Registrar asistencia
+@endsection
 
-        .header {
-            background-color: #fd6060;
-            /* height: 300px; */
-            justify-content: center;
-        }
+@section('header-title')
+Registra tu Check in / Check out
+@endsection
 
-        .content {
-            justify-content: center;
-        }
+@section('content')
+<div class="row px-0 mx-0 content">
+    <div class="col-lg-8 col-md-12 inner-content">
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-8">
 
-        .title-container {
-            margin-top: 95px;
-        }
-
-        @media (max-width: 420px) {
-            .title {
-            color: white;
-            font-size: 25px;
-            font-weight: 800;
-            display: block;
-            }
-        }
-
-        @media (min-width: 421px) {
-            .title {
-            color: white;
-            font-size: 35px;
-            font-weight: 800;
-            display: block;
-            }
-        }
-        
-
-        .inner-content {
-            background-color: white;
-            height: 300px;
-            margin-top: -100px;
-        }
-        
-        .spacing {
-            height: 130px;
-        }
-
-    </style>
-</head>
-
-<body>
-    <div class="container-fluid">
-
-        <div class="row header">
-            <div class="col-lg-10 text-center title-container">
-                @if ($company->logo)
-                    <img src="{{ $company->logo }}" alt="" style="max-height: 70px">
-                @else 
-                    <span class="title">{{ $company->name }}</span>
+                @if (session('error'))
+                    <div class="alert alert-warning mx-3" role="alert">
+                        {{ session('error') }}
+                    </div> 
                 @endif
-                <span class="title">Registra tu Check in / Check out</span>
-            </div>
-            <div class="col-lg-12 spacing"></div>
-        </div>
-
-        <div class="row px-0 mx-0 content">
-            <div class="col-lg-8 col-md-12 inner-content">
-                <div class="row mt-4 justify-content-center">
-                    <div class="col-md-8">
-
-                        @if (session('error'))
-                            <div class="alert alert-warning mx-3" role="alert">
-                                {{ session('error') }}
-                            </div> 
-                        @endif
-                        
-                        <form action="{{ route('attendance.post', [ 'company' => $company->id ]) }}" method="POST">
-                            @csrf
-                            <div class="form-group col-md-12">
-                                <input 
-                                    type="text" 
-                                    class="form-control form-control-lg" 
-                                    name="rut"
-                                    placeholder="Ingresa tu RUT"
-                                    id="rut"
-                                    required
-                                >
-                            </div>
-                            <div class="form-group col-md-12">
-                                <button type="submit" class="btn btn-primary btn-lg btn-block">
-                                    Registrar asistencia
-                                </button>
-                            </div>
-                        </form>
+                
+                <form action="{{ route('attendance.post', [ 'company' => $company->id ]) }}" method="POST">
+                    @csrf
+                    <div class="form-group col-md-12">
+                        <input 
+                            type="text" 
+                            class="form-control form-control-lg" 
+                            name="rut"
+                            placeholder="Ingresa tu RUT"
+                            id="rut"
+                            value="{{ old('rut') }}"
+                            required
+                        >
                     </div>
-                </div>
+                    <div class="form-group col-md-12">
+                        <button type="submit" class="btn btn-primary btn-lg btn-block">
+                            Registrar asistencia
+                        </button>
+                    </div>
+
+                    <input type="hidden" name="confirm" value="{{  request()->get('confirm') ?? '0'  }}">
+                </form>
             </div>
         </div>
     </div>
-
-    <script src="{{ asset('vendor/jquery/dist/jquery.slim.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-
-    {{-- Rut Formatter --}}
-    <script src="{{ asset('js/rut-formatter.js') }}"></script>
-    <script>
-        $('#rut').rut();
-    </script>
-</body>
-
-</html>
+</div>
+@endsection
--- resources/views/attendance/post.blade.php
+++ resources/views/attendance/post.blade.php
@@ -1,106 +1,29 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('attendance.layout.base')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Asistencia registrada - {{ env('APP_NAME') }}</title>
-    <link rel="stylesheet" media="screen" href="{{ asset('css/theme.css') }}">
-    <style>
-        body {
-            background-color: #fbf9f4;
-        }
+@section('title')
+Asistencia registrada
+@endsection
 
-        .header {
-            background-color: #fd6060;
-            /* height: 300px; */
-            justify-content: center;
-        }
+@section('header-title')
+    @if ($attendance->entry_type === $typeCheckIn)
+    ¡Check In registrado!
+    @endif
+    @if ($attendance->entry_type === $typeCheckOut)
+    ¡Check Out registrado!
+    @endif
+@endsection
 
-        .content {
-            justify-content: center;
-        }
-
-        .title-container {
-            margin-top: 95px;
-        }
-
-        .title {
-            color: white;
-            font-size: 35px;
-            font-weight: 800;
-            display: block;
-        }
-
-        @media (max-width: 420px) {
-            .title {
-            color: white;
-            font-size: 25px;
-            font-weight: 800;
-            display: block;
-            }
-        }
-
-        @media (min-width: 421px) {
-            .title {
-            color: white;
-            font-size: 35px;
-            font-weight: 800;
-            display: block;
-            }
-        }
-
-        .inner-content {
-            background-color: white;
-            height: 300px;
-            margin-top: -100px;
-        }
-
-        .spacing {
-            height: 130px;
-        }
-
-    </style>
-</head>
-
-<body>
-    <div class="container-fluid">
-
-        <div class="row header">
-            <div class="col-lg-10 text-center title-container">
-                @if ($company->logo)
-                    <img src="{{ $company->logo }}" alt="" style="max-height: 70px">
-                @else 
-                    <span class="title">{{ $company->name }}</span>
-                @endif
-                <span class="title">
-                    @if ($attendance->entry_type === $typeCheckIn)
-                        ¡Check In registrado!
-                    @endif
-                    @if ($attendance->entry_type === $typeCheckOut)
-                        ¡Check Out registrado!
-                    @endif
-                </span>
-            </div>
-            <div class="col-lg-12 spacing"></div>
-        </div>
-
-        <div class="row px-0 mx-0 content">
-            <div class="col-lg-8 col-md-12 inner-content">
-                <div class="row mt-4 justify-content-center">
-                    <div class="col-md-8">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><b>Cliente</b>: {{ $attendance->customer->first_name  . ' ' . $attendance->customer->last_name }}</li>
-                            <li class="list-group-item"><b>Hora marcada</b>: {{ $attendance->attendance_time->format('h:i A') }}</li>
-                          </ul>
-                    </div>
-                </div>
+@section('content')
+<div class="row px-0 mx-0 content">
+    <div class="col-lg-8 col-md-12 inner-content">
+        <div class="row mt-4 justify-content-center">
+            <div class="col-md-8">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><b>Cliente</b>: {{ $attendance->customer->first_name  . ' ' . $attendance->customer->last_name }}</li>
+                    <li class="list-group-item"><b>Hora marcada</b>: {{ $attendance->attendance_time->format('h:i A') }}</li>
+                  </ul>
             </div>
         </div>
     </div>
-
-    <script src="{{ asset('vendor/jquery/dist/jquery.slim.min.js') }}"></script>
-    <script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
-</body>
-
-</html>
+</div>
+@endsection
