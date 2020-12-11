5ff18c83cc6039afb6e261a09c17fee683d4e602
Sebastian Torres <sebastian.torres@twgroup.cl>
2020-12-10 21:27:35 -0300

    Change components to jquery scripts

--- app/Http/Livewire/Pos/Pos.php
+++ app/Http/Livewire/Pos/Pos.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Livewire\Pos;
 
-use App\Models\SaleBox;
-use App\Models\SalesBox;
 use App\Models\Seller;
 use Livewire\Component;
 
@@ -11,9 +9,11 @@ class Pos extends Component
 {
     public $seller;
     public $viewMode;
+    public $cash;
 
     protected $listeners = [
         'viewModeChanged' => 'setView',
+        'foo' => 'foo',
     ];
 
     public function render()
@@ -30,5 +30,15 @@ class Pos extends Component
     public function setView($view = null)
     {
         $this->viewMode = $view;
+    }
+
+    public function foo()
+    {
+        dd($this->cash);
+    }
+
+    public function updateCash($value)
+    {
+        $this->cash = $value;
     }
 }
