23ed29962963ebc103ab2c333c23d2b87685bea6
Julio Barragan <jcbarragan1994@gmail.com>
2020-12-15 08:53:53 -0400

    validations

--- app/Observers/PaymentsObserver.php
+++ app/Observers/PaymentsObserver.php
@@ -33,33 +33,34 @@ class PaymentsObserver
     {
         $currentDataPayments = Payments::find($payments->id);
         if (isset($payments->data_payment['payment_method'])) {
-
             if($currentDataPayments->data_payment != null){
                 $currentData = $currentDataPayments->data_payment;
                 $keyArr = array_keys($currentData);
                 $endKey = end($keyArr); 
-                $tmpCurrentDataPayment[$endKey+1] = $payments->data_payment;
-                $tmpDataFee[$endKey+1] = $payments->data_fee;
-                $tmpDataFee[$endKey+1]['data_fee'][$tmpCurrentDataPayment[$endKey+1]['id_fee']]['status_fee'] = 2;
-                $tmpCurrentDataPayment[$endKey+1]['id_fee'] = $tmpDataFee[$endKey+1]['data_fee'][$tmpCurrentDataPayment[$endKey+1]['id_fee']]['id_fee'];
+                $tmpCurrentDataPayment[$endKey] = $payments->data_payment;
+                $tmpDataFee[$endKey] = $payments->data_fee;
+                $tmpDataFee[$endKey]['data_fee'][$tmpCurrentDataPayment[$endKey]['id_fee']]['status_fee'] = 2;
+                $tmpCurrentDataPayment[$endKey]['id_fee'] = $tmpDataFee[$endKey]['data_fee'][$tmpCurrentDataPayment[$endKey]['id_fee']]['id_fee'];
                 $arrResult = array_merge($currentData,$tmpCurrentDataPayment);
                 $payments->data_payment = $arrResult;
-                $payments->data_fee = $tmpDataFee[$endKey+1];
-                $payments->amount_paid += $tmpCurrentDataPayment[$endKey+1]['amount_payment']; 
+                $payments->data_fee = $tmpDataFee[$endKey];
+                $payments->amount_paid += $tmpCurrentDataPayment[$endKey]['amount_payment']; 
                 if($payments->amount_paid == $payments->amount_total){
                     $payments->status = 2;
                 }
             }else{
-                
-                $tmpDataPayment[0] = $payments->data_payment;
+                $tmpDataPayment[$payments->data_payment['id_fee']] = $payments->data_payment;
                 $tmpDataFee = $payments->data_fee;
-                $tmpDataFee['data_fee'][0]['status_fee'] = 2;
-                $idFee = $tmpDataFee['data_fee'][0]['id_fee'];
-                $tmpDataPayment[0]['id_fee'] = $idFee;
-                $payments->amount_paid = $tmpDataFee['data_fee'][0]['amount'];
+                $tmpDataFee['data_fee'][$payments->data_payment['id_fee']]['status_fee'] = 2;
+                $idFee = $tmpDataFee['data_fee'][$payments->data_payment['id_fee']]['id_fee'];
+                $tmpDataPayment[$payments->data_payment['id_fee']]['id_fee'] = $idFee;
+                $payments->amount_paid = $tmpDataFee['data_fee'][$payments->data_payment['id_fee']]['amount'];
                 $payments->data_payment = $tmpDataPayment;          
                 $payments->data_fee = $tmpDataFee;     
-            }
+                if($payments->amount_paid == $payments->amount_total){
+                    $payments->status = 2;
+                }
+            } 
         }else{
             $paymentData = $payments->data_payment;
             $paymentData = $currentDataPayments->data_payment;
--- public/js/custom-backpack.js
+++ public/js/custom-backpack.js
@@ -2,7 +2,18 @@ $(function(){
     let URL = window.location;
     let acumTotal = 0;
 
-    $('body').on('submit',function(){
+    if($('.status-register').val() == 2){
+        $('.add-repeatable-element-button').hide();
+    }
+
+    $('body').on('click','.add-repeatable-element-button',function(){
+        $('.id-fee').each(function(value,element){
+            $(this).find('input').attr("name","id_fee").val(value);
+        })
+    });
+
+    $('body').on('submit',function(e){
+       
         if (URL.pathname.includes("/admin/payments")){
             $('.number-fee').val($('.repeatable-element').length)          
         }
@@ -15,12 +26,6 @@ $(function(){
     });
 
     $('a[href = "#tab_programar-pagos"]').click(function(){
-        if($('.repeatable-element').length == 1 ){
-            $('.id-fee').each(function(e){
-                $(this).find("input").val(e)
-            })
-        }
-
         let totalFee = 0;
         $('.field-amount').each(function(){
             let amount = $(this).find('input').val();
@@ -31,31 +36,20 @@ $(function(){
     });
 
     $('a[tab_name = "programar-pagos"]').on('click',function(){
-        if($('.repeatable-element').length > 0 ){
-            console.log($('.id-fee').length, 'idfee')
+        /* if($('.repeatable-element').length > 0 ){
             $('.id-fee').each(function(value,element){
+                console.log(value,element)
                 $(this).find('input').val(value);
             })
-        }
+        } */
+       
+        $('.field-status').each(function(){
+            if($(this).find('input').val() == 2){
+                $(this).parent('.repeatable-element').find('.delete-element').hide()                
+                $(this).parent('.repeatable-element').find('.field-date').find('input').attr('readonly', true)                
+                $(this).parent('.repeatable-element').find('.field-amount').find('input').attr('readonly', true)                
+            }
+        });
     })
 
-    $('.add-repeatable-element-button').click(function(e){
-
-   /*      $('.field-amount').each(function(val){
-            let amount = $(this).find('input').val();
-            acumTotal = parseFloat(acumTotal) + parseFloat(amount);
-        });
-        $('.total-fee').val(acumTotal) */
-
-        if(URL.hash == '#programar-pagos'){
-           // console.log($('.repeatable-element').length,'len')
-            if($('.repeatable-element').length > 0 ){
-                $('.id-fee').each(function(value,element){
-                   // console.log(value,element,'value,element')
-                    $(this).addClass('id-f-'+value);
-                    //$(this).find("input").val(value+1)
-                })
-            }
-        }
-    });
 });
