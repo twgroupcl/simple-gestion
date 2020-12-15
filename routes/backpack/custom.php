5d675176f9cfe44a27a320fa63483b62bbec1ef5
Julian Murphy <julian.murphy@twgroup.cl>
2020-12-14 22:02:55 -0300

    fix and unify download pdf

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -73,8 +73,7 @@ Route::group([
      Route::get('invoice/{invoice}/create-real-document', 'ManageInvoiceCrudController@createRealDocument');
      Route::get('invoice/{invoice}/delete-temporary-document', 'ManageInvoiceCrudController@deleteTemporaryDocument');
      Route::get('invoice/{invoice}/to-manage', 'ManageInvoiceCrudController@index');
-     Route::get('invoice/{invoice}/get-temporal-pdf', 'ManageInvoiceCrudController@getTemporalPDF');
-     Route::get('invoice/{invoice}/get-real-pdf', 'ManageInvoiceCrudController@getRealPDF');
+     Route::get('invoice/{invoice}/get-pdf', 'ManageInvoiceCrudController@getPDF');
      Route::get('invoice/{invoice}/issue-credit-note', 'ManageInvoiceCrudController@issueCreditNote');
      Route::get('invoice-type/{invoiceType}/get-code', 'InvoiceTypeCrudController@getCode');
     // API routes
