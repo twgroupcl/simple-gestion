050a2032868bc95f7c7f5d6f92b79870a6721d8e
Jose Alejandro <dluccajose@gmail.com>
2020-12-23 12:42:15 -0400

    add param tipoPapel to getRealPDF

--- app/Services/DTE/DTEService.php
+++ app/Services/DTE/DTEService.php
@@ -103,7 +103,7 @@ class DTEService
         return self::exec($url, [], $headers, $method);
     }
 
-    public function getRealPDF(Invoice $invoice)
+    public function getRealPDF(Invoice $invoice, $tipoPapel = 0)
     {
         $method = 'GET';
         $customerUid = rutWithoutDV($invoice->uid); 
@@ -113,7 +113,7 @@ class DTEService
             $invoice->invoice_type->code . '/' . 
             $invoice->folio . '/' .
             $sellerUid . 
-            '?papelContinuo=0&copias_tributarias=1&copias_cedibles=1&cedible=0&compress=0&base64=0';
+            '?papelContinuo=' . $tipoPapel . '&copias_tributarias=1&copias_cedibles=1&cedible=0&compress=0&base64=0';
 
         $headers = $this->headers;
 
