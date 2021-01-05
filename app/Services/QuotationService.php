2066873157a4e796504c372479ea9df0dde2fdbf
Jose Alejandro <dluccajose@gmail.com>
2021-01-05 18:43:55 -0400

    optional array for generateRecurrentQuotations method

--- app/Services/QuotationService.php
+++ app/Services/QuotationService.php
@@ -26,18 +26,24 @@ class QuotationService {
      * Search all recurrent quotations and generate the next quotation base on the "Next due date" field. This function is 
      * meant to be on a daily cron job
      * 
+     * @param Carbon $date
+     * @param array $quotationsArrayId 
+     * @return void
      */
-    public function generateRecurrentQuotations(Carbon $date = null)
+    public function generateRecurrentQuotations(Carbon $date = null, $quotationsArrayId = null)
     {
         if (is_null($date)) $date = new Carbon();
-        //dd($date);
+
         // Buscar todas las cotizaciones recurrentes con estado aprobada
         $quotations = Quotation::where([
                 'quotation_status' => Quotation::STATUS_ACCEPTED, 
-                'is_recurring' => true ])
-            ->with('childrens')
-            ->get();
+                'is_recurring' => true ])->with('childrens');
+        
+        if (!is_null($quotationsArrayId) && !empty($quotationsArrayId)) {
+            $quotations = $quotations->whereIn('id', $quotationsArrayId);
+        }
 
+        $quotations = $quotations->get();
         foreach ($quotations as $quotation) {
 
             DB::beginTransaction();
@@ -48,7 +54,6 @@ class QuotationService {
                 // Verificar criterio de parada
                 // Si el criterio de parada se cumple, cambiar el estado de la cotizacion
                 if ($recurringData['end_type'] === 'date') {
-                    //dd('aq');
                     $endDate = new Carbon($recurringData['end_date']);
                     if ($date > $endDate) {
                         $quotation->quotation_status = 'Completada'; // Que estado cambiar?
@@ -57,9 +62,7 @@ class QuotationService {
                         continue;
                     }
                 } else if ($recurringData['end_type'] === 'repetition') {
-                    
                     if (intval($recurringData['end_after_reps']) <= $quotation->childrens->count()) {
-                       // dd('repetition');
                         $quotation->quotation_status = 'Completada'; // Que estado cambiar?
                         $quotation->updateWithoutEvents();
                         DB::commit();
@@ -71,13 +74,13 @@ class QuotationService {
                 $nextDueDate = new Carbon($quotation->next_due_date);
 
                 if ($nextDueDate->format('Y-m-d') !== $date->format('Y-m-d')) {
-                    //dd('123');
+                    DB::commit();
                     continue;
                 }
 
                 // Revisar que no exista una cotizacion hija ese dia
                 if ($this->existChildrenQuotationOnDate($quotation->childrens, $date)) {
-                   // dd('345');
+                    DB::commit();
                     continue;
                 }
 
@@ -96,6 +99,8 @@ class QuotationService {
                 $newQuotation->save();
 
                 // Modificar "next due date" de la cotizacion padre sumando el ciclo de recurrencia
+                // TODO: verificar el nuevo next due date, para que la cotizacion pueda cambiar a completada
+                // sin necesidad de esperar al proximo next due date para que ocurra la verificacion
                 $newNextDueDate = $nextDueDate->add(intval($recurringData['repeat_number']), $recurringData['repeat_every']);
                 $quotation->next_due_date = $newNextDueDate;
                 $quotation->updateWithoutEvents();
@@ -105,9 +110,7 @@ class QuotationService {
                 DB::rollback();
                 Log::error($e->getMessage(), [ 'quotation_id' => $quotation->id]);
             }
-            
         }
-            
     }
 
 
@@ -119,7 +122,6 @@ class QuotationService {
             $childDate = new Carbon($child->created_at);
             if ($childDate->format('Y-m-d') === $date->format('Y-m-d')) $exits = true;
         }
-
 
         return $exits;
     }
