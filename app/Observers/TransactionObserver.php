12811de9f9c3ab79a28a0a0b68b03a6d8ea55005
Julian Murphy <julian.murphy@twgroup.cl>
2021-01-07 17:39:16 -0300

    sync details in transaction

--- app/Observers/TransactionObserver.php
+++ app/Observers/TransactionObserver.php
@@ -2,7 +2,7 @@
 
 namespace App\Observers;
 
-use App\Models\Transaction;
+use App\Models\{ Transaction, TransactionDetail };
 
 class TransactionObserver
 {
@@ -10,5 +10,39 @@ class TransactionObserver
     {
         $company = backpack_user()->current()->company;
         $transaction->company_id = $company->id;
+    }
+
+    public function created(Transaction $transaction)
+    {
+        $this->syncTransactionDetails($transaction);
+    }
+
+
+    public function updated(Transaction $transaction)
+    {
+        $this->syncTransactionDetails($transaction);
+    }
+
+    public function syncTransactionDetails($transaction, $options = []) {
+
+        if ( !empty($transaction->json_transaction_details) ) {
+
+            TransactionDetail::where('transaction_id', $transaction->id)->delete();
+
+            $details = is_string($transaction->json_transaction_details) ? 
+                    json_decode($transaction->json_transaction_details, true) : 
+                    $transaction->json_transaction_details;
+
+            foreach($details as &$detail) {
+
+                // Sanitize numbers
+                $detail['value'] = sanitizeNumber($detail['value']);
+                $props = [
+                    'value' => $detail['value'],
+                    'transaction_id' => $transaction->id
+                ];
+                TransactionDetail::create($detail);
+            }
+        }
     }
 }
