95872974c4c8c2969103bc33056be2631adcf174
Julian Murphy <julian.murphy@twgroup.cl>
2021-02-08 19:20:56 -0300

    delete white spaces

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -156,6 +156,7 @@
 @can('plans.list')
 <li class='nav-item'><a class='nav-link' href='{{ backpack_url('plans') }}'><i class='nav-icon la la-question'></i> Plans</a></li>
 @endcan
+
 @canany(['support.list'])
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon la la-money-bill"></i> Servicio al cliente</a>
