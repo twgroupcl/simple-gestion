b5377ae46f5f6581c6b68c112e283430418761e5
Jose Alejandro <dluccajose@gmail.com>
2021-02-25 15:28:36 -0400

    added migration for price list and price list item

---
+++ database/migrations/2021_02_25_155956_create_price_lists_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePriceListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('price_lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->nullable();
+            $table->string('name');
+            $table->date('apply_on_date')->nullable();
+            $table->longText('intial_options')->nullable();
+            $table->foreignId('user_id')->constrained();
+            $table->boolean('status')->default(true);
+            $table->foreignId('company_id')->constrained();
+            $table->longText('json_value')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('price_lists');
+    }
+}
