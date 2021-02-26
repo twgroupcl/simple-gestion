a094b88c32429ca4ec8d90a028295c7d3225e192
Jose Alejandro <dluccajose@gmail.com>
2021-02-26 15:56:41 -0400

    fix special price range

--- resources/views/livewire/products/card.blade.php
+++ resources/views/livewire/products/card.blade.php
@@ -67,8 +67,8 @@
                 @endif  
             @else
             <div class="product-price">
-                @if($product->special_price)
-                <span class="text-accent color-orange">{{ currencyFormat($product->special_price, defaultCurrency(), true) }}</span>
+                @if ($product->has_special_price)
+                <span class="text-accent">{{ currencyFormat($product->special_price, defaultCurrency(), true) }}</span>
                 <del class="font-size-sm text-muted"><small>{{ currencyFormat($product->price, defaultCurrency(), true) }}</small></del>
                 @else
                 <span class="text-accent color-orange">{{ currencyFormat($product->real_price, defaultCurrency(), true) }}</span>
