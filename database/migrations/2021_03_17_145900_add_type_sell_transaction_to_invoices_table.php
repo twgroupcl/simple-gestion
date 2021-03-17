3011164fcda8c0727a39822a9751cb946b4e1c83
Julian Murphy <julian.murphy@twgroup.cl>
2021-03-17 15:09:42 -0300

    migrations excempts and type sell transaction

---
+++ database/migrations/2021_03_17_145900_add_type_sell_transaction_to_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTypeSellTransactionToInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            //
+            $table->smallInteger('type_sell_transaction')
+                  ->unsigned()
+                  ->comment('Type sell transaction. 1, 2 or 3. Ref: TpoTranVenta');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn('type_sell_transaction');
+        });
+    }
+}
