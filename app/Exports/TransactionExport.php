e7d61a16cd31bbf3de0d8a56e9e701a8edc830e0
Julian Murphy <julian.murphy@twgroup.cl>
2021-03-26 14:04:32 -0300

    add columns export data

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -24,6 +24,8 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
         return $this->transactions->map(function ($transaction) {
             $transactionArray = [];
 
+            $jsonValues = is_array($transaction->json_value) ? $transaction->json_value : json_decode($transaction->json_value, true);
+            
             foreach ($transaction->transaction_details as $detail) {
 
                 if (isset($detail->document_model)) {
@@ -34,16 +36,24 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
                     $dte = $detail->document_identifier;
                 }
 
+                if (isset($detail->json_detail)) {
+                    $metadataDetail = is_array($detail->json_detail) ? $detail->json_detail : json_decode($detail->json_detail, true);
+                    $notes = $metadataDetail['notes'] ?? '';
+                    $documentType = $metadataDetail['document_type'] ?? '';
+                }
                 $transactionArray[] = [
                     'id' => $transaction->id,
                     'value' => $detail->value,
                     'document' => $dte,
+                    'document_type' => $documentType,
                     'transaction_type' => $transaction->transaction_type->name,
                     'is_payment' => $transaction->transaction_type->is_payment ? 'Abono' : 'Gasto',
                     'accounting_account_description' => $transaction->accounting_account->name ?? null,
                     'accounting_account_code' => $transaction->accounting_account->code ?? null,
                     'bank_account' => $transaction->bank_account->account_number,
                     'note' => $transaction->note,
+                    'person_name_reference' => isset($jsonValues) && array_key_exists('person_name_reference', $jsonValues) ? $jsonValues['person_name_reference'] : '',
+                    'person_uid_reference' => isset($jsonValues) && array_key_exists('person_uid_reference', $jsonValues) ? $jsonValues['person_uid_reference'] : '',
                     'date' => $transaction->date,
                 ];
             }
@@ -63,6 +73,7 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
                 'ID',
                 'Numero de cuenta bancaria',
                 'Documento',
+                'Tipo de documento',
                 'Monto',
                 'Gasto/Abono',
                 'Tipo de transaccion',
@@ -70,6 +81,8 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
                 'Código plan de c.',
                 'Nota',
                 'Fecha',
+                'RUT referencia',
+                'Nomb. referencia',
             ],
         ];
     }
@@ -80,6 +93,7 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
             $document['id'],
             $document['bank_account'],
             $document['document'],
+            $document['document_type'] ?? '',
             $document['value'],
             $document['transaction_type'],
             $document['is_payment'],
@@ -87,6 +101,8 @@ class TransactionExport implements FromArray, WithMapping , WithHeadings
             $document['accounting_account_code'],
             $document['note'],
             $document['date'],
+            $document['person_uid_reference'] ?? '',
+            $document['person_name_reference'] ?? '',
         ];
     }
 }
