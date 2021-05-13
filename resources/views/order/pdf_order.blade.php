d5acde0b70ecad4e99e2043995130967cf67f460
Jose Alejandro <dluccajose@gmail.com>
2021-05-13 10:22:49 -0400

    change desing of order pdf

--- resources/views/order/pdf_order.blade.php
+++ resources/views/order/pdf_order.blade.php
@@ -6,14 +6,14 @@ $addressData = $order->json_value;
 
 
 $addressShipping = null;
-if(isset($addressData->addressInvoice)){
-$addressShipping = json_decode($addressData->addressShipping);
+if(isset($addressData['addressShipping'])){
+$addressShipping = $addressData['addressShipping'];
 }
 
 
 $addressInvoice = null;
-if(isset($addressData->addressInvoice)){
-$addressInvoice = json_decode($addressData->addressInvoice);
+if(isset($addressData['addressInvoice']) && !empty($addressData['addressInvoice']->last_name)){
+$addressInvoice = $addressData['addressInvoice'];
 }
 
 $communeShipping = null;
@@ -26,8 +26,6 @@ $communeShipping = Commune::where('id', $addressShipping->address_commune_id)->f
 if($addressInvoice){
 $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->first();
 }
-
-
 
 @endphp
 {{--
@@ -102,8 +100,24 @@ $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->fir
         left: 0;
         bottom: -10;
         width: 100%;
-
         text-align: center;
+    }
+
+    .resume-table {
+        background: #f6f9fc;
+        width: 100%;
+        font-size: 15px;
+        border-radius: 4px 4px 4px 4px;
+        padding: 20px;
+        margin-top: 35px;
+        margin-bottom: 35px;
+    }
+    
+    .address-section {
+        border: 1px solid #e3e9ef !important;
+        border-radius: 0.4375rem;
+        padding-left: 15px;
+        margin: 5px;
     }
 
 </style>
@@ -111,9 +125,48 @@ $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->fir
 {{-- @section('content') --}}
 
 
-<div style="background: #FDCA00; height: 120px; padding-top: 25px; padding-left: 10px">
+<div style="background: #FDCA00; height: 100px; padding-top: 25px; padding-left: 10px">
     <img src="{{ public_path() . '/img/covepa-logo.png' }}" alt="Covepa" height="80px">
 </div>
+
+<br>
+
+<table width="100%" cellspacing="5px">
+    <tr>
+        <td width="50%"class="address-section" >
+            @if ($addressShipping)
+                <div class="direccion-facturacion-titulo">
+                    <p><strong>Dirección de envío</strong></p>
+                </div>
+                <p>
+                <p class="p-estrecho">{{ $order->first_name . ', ' . $order->last_name }}</p>
+                <p class="p-estrecho">{{ $addressShipping->address_street . ', ' . $addressShipping->address_number }}
+                </p>
+                <p class="p-estrecho">{{ $communeShipping->name }}</p>
+                <p class="p-estrecho">Teléfono: {{ $order->cellphone }}</p>
+                </p>
+            @endif
+        </td>
+        <td width="50%"  @if ($addressInvoice)  class="address-section" @endif>
+            @if ($addressInvoice)
+                <div class="direccion-facturacion-titulo">
+                    <p><strong>Direccion de facturación </strong></p>
+                </div>
+                <p>
+                <p class="p-estrecho">{{ $addressInvoice->first_name . ', ' . $addressInvoice->last_name }}</p>
+                <p class="p-estrecho">{{ $addressInvoice->address_street . ', ' . $addressInvoice->address_number }}</p>
+                <p class="p-estrecho">
+                    @if ($communeInvoice) 
+                    {{ $communeInvoice->name }}
+                    @endif
+                </p>
+                <p class="p-estrecho">Teléfono: {{ $addressInvoice->cellphone }}</p>
+                </p>
+            @endif
+        </td>
+    </tr>
+
+</table>    
 
 <table width="100%" style="margin-top: 25px;">
     <tr>
@@ -130,41 +183,6 @@ $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->fir
 
 </table>
 
-<table width="100%">
-    <tr>
-        <td width="50%">
-            @if ($addressShipping)
-                <div class="direccion-facturacion-titulo">
-                    <p><strong>Dirección de envío</strong></p>
-                </div>
-                <p>
-                <p class="p-estrecho">{{ $order->first_name . ', ' . $order->last_name }}</p>
-                <p class="p-estrecho">{{ $addressShipping->address_street . ', ' . $addressShipping->address_number }}
-                </p>
-                <p class="p-estrecho">{{ $communeShipping->name }}</p>
-                <p class="p-estrecho">Teléfono: {{ $order->cellphone }}</p>
-                </p>
-            @endif
-        </td>
-        <td width="50%">
-            @if ($addressInvoice)
-                <div class="direccion-facturacion-titulo">
-                    <p><strong>Direccion de facturación </strong></p>
-                </div>
-                <p>
-                <p class="p-estrecho">{{ $addressInvoice->first_name . ', ' . $addressInvoice->last_name }}</p>
-                <p class="p-estrecho">{{ $addressInvoice->address_street . ', ' . $addressInvoice->address_number }}</p>
-                <p class="p-estrecho">
-                    @if ($communeInvoice) {{ $communeInvoice->name }}</beautify
-                        end=" @endif">
-                </p>
-                <p class="p-estrecho">Teléfono: {{ $addressInvoice->cellphone }}</p>
-                </p>
-            @endif
-        </td>
-    </tr>
-
-</table>
 <h3 class="h3-titulo"> Productos</h3>
 <table width="100%">
     @php
@@ -180,12 +198,13 @@ $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->fir
         // $total += ($item->price * $item->qty) + ($item->shipping_total * $item->qty);
         @endphp
         <tr class="product-item">
-            <td width="30%" class="product-item">
+            <td width="15%" class="product-item">
+                
+                <img class="width-5" src="{{ public_path() . '/' . $product->getFirstImagePath() }}" width="80%">
+            </td>
+            <td width="40%" class="product-item" style="line-height: 5px">
                 <p> {{$product->seller->visible_name}}</p>
                 <p><strong>{{ $product->name }}</strong></p>
-                <img class="width-5" src="{{ public_path() . '/' . $product->getFirstImagePath() }}" width="15%">
-            </td>
-            <td width="40%" class="product-item">
                 <p><strong>Precio : </strong>{{ currencyFormat($item->price ? $item->price : 0, 'CLP', true) }}
                 </p>
                 <p><strong>Cantidad : </strong>{{ $item->qty }}</p>
@@ -206,40 +225,40 @@ $communeInvoice = Commune::where('id', $addressInvoice->address_commune_id)->fir
             </td>
         </tr>
     @endforeach
-    <tr>
-        <td colspan="3" width="100%" class="product-item" align="right">
-            <p><strong>Subtotal</strong> {{ currencyFormat($order->sub_total ? $order->sub_total : 0, 'CLP', true) }}</p>
-        </td>
-    </tr>
-    @if ($order->shipping_total != 0)
-        <tr>
-            <td colspan="3" width="100%" align="right">
-
-                <p><strong>Envío</strong>
-                    {{ currencyFormat($order->shipping_total ? $order->shipping_total : 0, 'CLP', true) }}
-                </p>
-
-
-            </td>
-        </tr>
-    @endif
-    <tr>
-        <td colspan="3" width="100%" align="right">
-            <p><strong>Total</strong> {{ currencyFormat($order->total ? $order->total : 0, 'CLP', true) }}</p>
-        </td>
-    </tr>
 </table>
-<br />
-<br />
+
+<div style="padding-right: 50px; padding-left: 50px">
+    <table class="resume-table">
+        <tbody>
+            <tr>
+                <td><strong>Subtotal</strong></td>
+                <td style="text-align: right"><strong>{{ currencyFormat($order->sub_total ? $order->sub_total : 0, 'CLP', true) }}</strong></td>
+            </tr>
+            @if ($order->shipping_total != 0)
+            <tr>
+                <td><strong>Envío</strong></td>
+                <td style="text-align: right"><strong>{{ currencyFormat($order->shipping_total ? $order->shipping_total : 0, 'CLP', true) }}</strong></td>
+            </tr>
+            @endif
+            <tr>
+                <td><strong>Método de pago</strong></td>
+                <td style="text-align: right"><strong>{{ $order->order_payments->first()->method_title }}</strong></td>
+            </tr>
+            <tr>
+                <td><strong>Total</strong></td>
+                <td style="text-align: right"><strong>{{ currencyFormat($order->total ? $order->total : 0, 'CLP', true) }}</strong></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+</section>
 <br />
 <br />
 <h3> Datos de pago</h3>
 <table width="100%" class="border-table">
-    <tr>
-        <td width="100%">
-            <p><strong> Método de pago seleccionado: </strong> {{ $order->order_payments->first()->method_title }}</p>
-        </td>
-    </tr>
     <tr>
         <td width="100%">
             <p><strong>Fecha de
