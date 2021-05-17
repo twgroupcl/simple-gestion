d50ddddf371e36d0afd424de9cb5309c03a2ec06
fkmurphy <njmdistrisoft@gmail.com>
2021-05-17 16:36:02 -0300

    hotfix backpack panel

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -24,7 +24,7 @@ return [
     // ----
 
     // Project name. Shown in the window title.
-    'project_name' => 'Simple Gestión',
+    'project_name' => 'ContigoPyme',
 
     // When clicking on the admin panel's top-left logo/name,
     // where should the user be redirected?
