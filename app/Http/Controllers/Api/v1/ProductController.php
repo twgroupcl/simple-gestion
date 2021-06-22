1a7bdd43eb29b87eaf9b43187550096828d779a7
Jose Alejandro <dluccajose@gmail.com>
2021-06-22 14:17:05 -0400

    disable stric checking

--- app/Http/Controllers/Api/v1/ProductController.php
+++ app/Http/Controllers/Api/v1/ProductController.php
@@ -312,7 +312,7 @@ class ProductController extends Controller
         if ($request['special_price_to']) $product->special_price_to = new DateTime($request['special_price_to']);
 
         if (! is_null($request['special_price'])) {
-            $product->special_price = $request['special_price'] === 0 ? null : $request['special_price'];
+            $product->special_price = $request['special_price'] == 0 ? null : $request['special_price'];
         }
 
         try {
